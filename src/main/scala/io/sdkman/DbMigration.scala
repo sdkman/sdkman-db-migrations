3126bdac3d80eff4e089b8a86bdea815eb4be600
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-02-13 08:15:54 +0100

    Add auth mechanism to remote mongo url.

--- src/main/scala/io/sdkman/DbMigration.scala
+++ src/main/scala/io/sdkman/DbMigration.scala
@@ -5,7 +5,7 @@ import com.github.mongobee.Mongobee
 object DbMigration extends App with Configuration {
 
   mongoUsernameO.fold(mongobee(s"mongodb://$mongoHost:$mongoPort")) { mongoUsername =>
-    mongobee(s"mongodb://$mongoUsername:$mongoPassword@$mongoHost:$mongoPort")
+    mongobee(s"mongodb://$mongoUsername:$mongoPassword@$mongoHost:$mongoPort/$mongoDatabase?authMechanism=SCRAM-SHA-1")
   }.setDbName(mongoDatabase)
     .setChangeLogsScanPackage("io.sdkman.changelogs")
     .execute()
