b0661946627a7a60d133a0bd3803993d1d17cf14
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-02-13 08:21:52 +0100

    Refine DbMigration connection code.

--- src/main/scala/io/sdkman/DbMigration.scala
+++ src/main/scala/io/sdkman/DbMigration.scala
@@ -4,11 +4,15 @@ import com.github.mongobee.Mongobee
 
 object DbMigration extends App with Configuration {
 
-  mongoUsernameO.fold(mongobee(s"mongodb://$mongoHost:$mongoPort")) { mongoUsername =>
-    mongobee(s"mongodb://$mongoUsername:$mongoPassword@$mongoHost:$mongoPort/$mongoDatabase?authMechanism=SCRAM-SHA-1")
+  mongoUsernameO.fold(localMongoConn) { mongoUsername =>
+    remoteMongoConn(mongoUsername)
   }.setDbName(mongoDatabase)
     .setChangeLogsScanPackage("io.sdkman.changelogs")
     .execute()
+
+  lazy val localMongoConn = mongobee(s"mongodb://$mongoHost:$mongoPort")
+
+  def remoteMongoConn(username: String) = mongobee(s"mongodb://$username:$mongoPassword@$mongoHost:$mongoPort/$mongoDatabase?authMechanism=SCRAM-SHA-1")
 
   private def mongobee(url: String) = new Mongobee(url)
 }
