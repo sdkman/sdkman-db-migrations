395c7dc2dd2a9fbab9654760f60a5875cfb381cd
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-03-09 09:03:31 +0000

    Add openjdk 9.0.4 for linux.

--- src/main/scala/io/sdkman/changelogs/Migrations.scala
+++ src/main/scala/io/sdkman/changelogs/Migrations.scala
@@ -10,7 +10,11 @@ import scala.collection.JavaConverters._
 class VersionsMigration {
 
   @ChangeSet(order = "001", id = "001-add_java_904_zulu", author = "marc0der")
-  def migrate(db: MongoDatabase) = db.getCollection("versions").insertMany(List[Document](
+  def migrate001(db: MongoDatabase) = db.getCollection("versions").insertMany(List[Document](
     CandidateVersion("java", "9.0.4-zulu", Some("MAC_OSX"), "https://cdn.azul.com/zulu/bin/zulu9.0.4.1-jdk9.0.4-macosx_x64.zip"),
     CandidateVersion("java", "9.0.4-zulu", Some("WINDOWS_64"), "https://cdn.azul.com/zulu/bin/zulu9.0.4.1-jdk9.0.4-win_x64.zip")).asJava)
+
+  @ChangeSet(order = "002", id = "002-add_java_904_zulu", author = "marc0der")
+  def migrate002(db: MongoDatabase) = db.getCollection("versions").insertOne(
+    CandidateVersion("java", "9.0.4-zulu", Some("LINUX_64"), "https://cdn.azul.com/zulu/bin/zulu9.0.4.1-jdk9.0.4-linux_x64.tar.gz"))
 }
