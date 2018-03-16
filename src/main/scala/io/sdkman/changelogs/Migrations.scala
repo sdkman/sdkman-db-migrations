02b345ad4b0972b334368ad406b5cb50e925c60f
Sven Marquardt <sven2qhotmail.de>
2018-03-16 19:38:41 +0100

    Added openjdk build
    Added all openjdk latest builds from adoptopenjdk.net refers to issue
    https://github.com/sdkman/sdkman-cli/issues/629
    	geändert:       src/main/scala/io/sdkman/changelogs/Migrations.scala

--- src/main/scala/io/sdkman/changelogs/Migrations.scala
+++ src/main/scala/io/sdkman/changelogs/Migrations.scala
@@ -1,6 +1,6 @@
 package io.sdkman.changelogs
 
-import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
+import com.github.mongobee.changeset.{ ChangeLog, ChangeSet }
 import com.mongodb.client.MongoDatabase
 import org.bson.Document
 
@@ -17,4 +17,20 @@ class VersionsMigration {
   @ChangeSet(order = "002", id = "002-add_java_904_zulu", author = "marc0der")
   def migrate002(db: MongoDatabase) = db.getCollection("versions").insertOne(
     CandidateVersion("java", "9.0.4-zulu", Some("LINUX_64"), "https://cdn.azul.com/zulu/bin/zulu9.0.4.1-jdk9.0.4-linux_x64.tar.gz"))
+
+  
+  @ChangeSet(order="004", id= "oo4-add_openjdk_version",author="MaSven")
+  def migrate004(db: MongoDatabase) = db.getCollection("versions").insertMany(List[Document](
+      CandidateVersion("java", "jdk9j9-latest-openjdk", Some("LINUX_64"), "https://api.adoptopenjdk.net/openjdk-opnj9/releases/x64_linux/latest/binary"),
+      CandidateVersion("java","jdk9-latest-opendjk",Some("LINUX_64"),"https://api.adoptopenjdk.net/openjdk9/releases/x64_linux/latest/binary"),
+      CandidateVersion("java","jdk9-latest-opendjk",Some("MAC_OSX"),"https://api.adoptopenjdk.net/openjdk9/releases/x64_mac/latest/binary"),
+      CandidateVersion("java","jdk9-latest-opendjk",Some("WINDOWS_64"),"https://api.adoptopenjdk.net/openjdk9/releases/x64_win/latest/binary"),
+      CandidateVersion("java","jdk8-latest-opendjk",Some("LINUX_64"),"https://api.adoptopenjdk.net/openjdk8/releases/x64_linux/latest/binary"),
+      CandidateVersion("java","jdk8-latest-opendjk",Some("MAC_OSX"),"https://api.adoptopenjdk.net/openjdk8/releases/x64_mac/latest/binary"),
+      CandidateVersion("java","jdk8-latest-opendjk",Some("WINDOWS_64"),"https://api.adoptopenjdk.net/openjdk8/releases/x64_win/latest/binary"),
+      CandidateVersion("java","jdk8j9-latest-opendjk",Some("LINUX_64"),"https://api.adoptopenjdk.net/openjdk8-opnj9/releases/x64_linux/latest/binary"),
+      CandidateVersion("java","jdk10-latest-opendjk",Some("LINUX_64"),"https://api.adoptopenjdk.net/openjdk10/releases/x64_linux/latest/binary"),
+      CandidateVersion("java","jdk10-latest-opendjk",Some("MAC_OSX"),"https://api.adoptopenjdk.net/openjdk10/releases/x64_mac/latest/binary")
+      ).asJava)
+  
 }
