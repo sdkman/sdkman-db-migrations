330adf15c6747c102a2a5eff9bd67d76a71386d3
Sven Marquardt <sven2q@hotmail.de>
2018-03-21 21:39:39 +0100

    Change the version suffix from adoptopenjdk
    Signed-off-by: Sven Marquardt <sven2q@hotmail.de>
    
    	geändert:       src/main/scala/io/sdkman/changelogs/Migrations.scala
    	geändert:       src/main/scala/io/sdkman/changelogs/package.scala

--- src/main/scala/io/sdkman/changelogs/Migrations.scala
+++ src/main/scala/io/sdkman/changelogs/Migrations.scala
@@ -1,10 +1,11 @@
 package io.sdkman.changelogs
 
-import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
+import com.github.mongobee.changeset.{ ChangeLog, ChangeSet }
 import com.mongodb.client.MongoDatabase
 import org.bson.Document
 
 import scala.collection.JavaConverters._
+import scala.collection.mutable.ListBuffer
 
 @ChangeLog(order = "001")
 class VersionsMigration {
@@ -35,10 +36,16 @@ class VersionsMigration {
   @ChangeSet(order = "005", id = "005-change_java_default_8u152_zulu", author = "MaSven")
   def migrate005(implicit db: MongoDatabase) = updateCandidateDefault("java", "8u152-zulu")
 
-
   @ChangeSet(order = "006", id = "003-add_maven_353", author = "hho")
   def migrate006(implicit db: MongoDatabase) = {
     insertVersions(CandidateVersion("maven", "3.5.3", Some("UNIVERSAL"), "https://archive.apache.org/dist/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.zip"))
     updateCandidateDefault("maven", "3.5.3")
+  }
+
+  @ChangeSet(order = "007", id = "007-change_openjdk_to_adoptopnjdk", author = "MaSven")
+  def migrate007(implicit db: MongoDatabase) = {
+    updateVersion("9u181-openjdk", "9u181-adoptopenjdk")
+    updateVersion("8u144-openjdk", "8u144-adoptopenjdk")
+    updateVersion("10u23-openjdk", "10u23-adoptopenjdk")
   }
 }
