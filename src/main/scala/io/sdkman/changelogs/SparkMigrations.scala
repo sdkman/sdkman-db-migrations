45e3c8e6dd1275d269f3a7a948afc22b3de4160d
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-04-19 16:45:08 +0200

    Fix compilation error.

--- src/main/scala/io/sdkman/changelogs/SparkMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/SparkMigrations.scala
@@ -5,7 +5,7 @@ import com.mongodb.client.MongoDatabase
 
 @ChangeLog(order = "007")
 class SparkMigrations {
-  @ChangeSet(order = "001", id = "001-add_spark_2.3.0")
+  @ChangeSet(order = "001", id = "001-add_spark_2.3.0", author = "marc0der")
   def migration001(implicit db: MongoDatabase) = {
     insertVersion(Version("spark", "2.3.0", "https://archive.apache.org/dist/spark/spark-2.3.0/spark-2.3.0-bin-hadoop2.7.tgz"))
     setCandidateDefault("spark", "2.3.0")
