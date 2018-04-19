d43a18387b4d7ff42b5e3a6207b31c84ec22047b
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-04-19 07:06:35 +0200

    Adds spark 2.3.0 as default.

---
+++ src/main/scala/io/sdkman/changelogs/SparkMigrations.scala
@@ -0,0 +1,13 @@
+package io.sdkman.changelogs
+
+import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
+import com.mongodb.client.MongoDatabase
+
+@ChangeLog(order = "007")
+class SparkMigrations {
+  @ChangeSet(order = "001", id = "001-add_spark_2.3.0")
+  def migration001(implicit db: MongoDatabase) = {
+    insertVersion(Version("spark", "2.3.0", "https://archive.apache.org/dist/spark/spark-2.3.0/spark-2.3.0-bin-hadoop2.7.tgz"))
+    setCandidateDefault("spark", "2.3.0")
+  }
+}
