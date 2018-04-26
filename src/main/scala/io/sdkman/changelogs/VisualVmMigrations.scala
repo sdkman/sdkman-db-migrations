95d8f168b5a1622f656a6f15e678ef0a0224828e
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-04-26 21:54:00 +0100

    Minor tweak.

--- src/main/scala/io/sdkman/changelogs/VisualVmMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/VisualVmMigrations.scala
@@ -8,9 +8,7 @@ class VisualVmMigrations {
 
   @ChangeSet(order = "001", id = "001-add_visualvm_1_4_1", author = "vpavic")
   def migration001(implicit db: MongoDatabase) = {
-    List(
-      Version("visualvm", "1.4.1", "https://github.com/visualvm/visualvm.src/releases/download/1.4.1/visualvm_141.zip")
-    ).insert()
+    Version("visualvm", "1.4.1", "https://github.com/visualvm/visualvm.src/releases/download/1.4.1/visualvm_141.zip").insert()
     setCandidateDefault("visualvm", "1.4.1")
   }
 
