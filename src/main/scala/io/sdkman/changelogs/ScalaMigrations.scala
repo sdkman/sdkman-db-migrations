07a422449e68aab3eb42023d40ac9b335c54a8b8
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-05-15 15:32:35 +0100

    Add scala 2.12.6

--- src/main/scala/io/sdkman/changelogs/ScalaMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/ScalaMigrations.scala
@@ -1,8 +1,19 @@
 package io.sdkman.changelogs
 
-import com.github.mongobee.changeset.ChangeLog
+import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
+import com.mongodb.client.MongoDatabase
 
 @ChangeLog(order = "003")
 class ScalaMigrations {
-  //TODO: Add new Scala migrations here
+
+  @ChangeSet(order = "001", id = "001-add_scala_2_12_6", author = "marc0der")
+  def migration001(implicit db: MongoDatabase) =
+    Version(
+      candidate = "scala",
+      version = "2.12.6",
+      url = "https://downloads.lightbend.com/scala/2.12.6/scala-2.12.6.zip"
+    ).validate()
+      .insert()
+      .asCandidateDefault()
+
 }
