1a0215394dccc718accffde6eceb16cf8c13e57e
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-05-27 23:07:38 +0100

    Tweaks to Micronaut candidate.

--- src/main/scala/io/sdkman/changelogs/MicronautMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/MicronautMigrations.scala
@@ -3,18 +3,15 @@ package io.sdkman.changelogs
 import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
 import com.mongodb.client.MongoDatabase
 
-@ChangeLog(order = "009")
+@ChangeLog(order = "010")
 class MicronautMigrations {
 
-  @ChangeSet(order = "001", id = "001_add_micronaut_1_0_0_M1", author = "jameskleeh")
+  @ChangeSet(order = "001", id = "001_add_micronaut_candidate", author = "jameskleeh")
   def migration001(implicit db: MongoDatabase) = {
-    insertCandidate(
-      Candidate(
-        candidate = "micronaut",
-        name = "Micronaut",
-        description = "Micronaut is an open source microservice framework for the JVM",
-        default = "1.0.0.M1",
-        websiteUrl = "http://micronaut.io/",
-        distribution = "UNIVERSAL"))
+    Candidate(
+      candidate = "micronaut",
+      name = "Micronaut",
+      description = "Micronaut is an open source microservice framework for the JVM",
+      websiteUrl = "http://micronaut.io/").insert()
   }
 }
