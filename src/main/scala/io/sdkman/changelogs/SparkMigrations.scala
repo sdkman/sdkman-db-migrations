fbd3786d531695f8749d4165636824880efd3c48
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-06-15 10:50:16 +0100

    Add spark 2.3.1

--- src/main/scala/io/sdkman/changelogs/SparkMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/SparkMigrations.scala
@@ -11,4 +11,12 @@ class SparkMigrations {
       .insert()
       .asCandidateDefault()
   }
+
+  @ChangeSet(order = "002", id = "002-add_spark_2.3.1", author = "marc0der")
+  def migration002(implicit db: MongoDatabase) = {
+    Version("spark", "2.3.1", "https://archive.apache.org/dist/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz")
+      .validateUrl()
+      .insert()
+      .asCandidateDefault()
+  }
 }
