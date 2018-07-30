b10d60525e14f51a1a0ff7182644253b205ed8d8
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-07-30 16:30:09 +0100

    Add Scala 2.11.11, 2.11.12

--- src/main/scala/io/sdkman/changelogs/ScalaMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/ScalaMigrations.scala
@@ -16,4 +16,17 @@ class ScalaMigrations {
       .insert()
       .asCandidateDefault()
 
+  @ChangeSet(order = "002", id = "002-add_scala_2_11_11_and_12", author = "marc0der")
+  def migration002(implicit db: MongoDatabase) =
+    List(
+      Version(
+        candidate = "scala",
+        version = "2.11.11",
+        url = "https://downloads.lightbend.com/scala/2.11.11/scala-2.11.11.zip"),
+      Version(
+        candidate = "scala",
+        version = "2.11.12",
+        url = "https://downloads.lightbend.com/scala/2.11.12/scala-2.11.12.zip"))
+      .validate()
+      .insert()
 }
