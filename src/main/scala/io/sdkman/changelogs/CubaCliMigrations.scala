5270502cb47f16ebe33cd6e44a417e0d6e53baec
Yuriy Artamonov <artamonov@haulmont.com>
2018-08-09 13:36:19 +0400

    Add CUBA CLI migrations - use unified cuba-cli name

--- src/main/scala/io/sdkman/changelogs/CubaCliMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/CubaCliMigrations.scala
@@ -9,7 +9,7 @@ class CubaCliMigrations {
   @ChangeSet(order = "001", id = "001_add_cubacli_candidate", author = "jreznot")
   def migration001(implicit db: MongoDatabase) = {
     Candidate(
-      candidate = "cubacli",
+      candidate = "cuba-cli",
       name = "CUBA CLI",
       description = "CUBA CLI is an open source command line utility that enables you to easily create projects based on CUBA Platform",
       websiteUrl = "https://cuba-platform.com",
@@ -18,19 +18,19 @@ class CubaCliMigrations {
 
     List(
       Version(
-        candidate = "cubacli",
+        candidate = "cuba-cli",
         version = "1.0.1",
         url = "https://bintray.com/cuba-platform/tools/download_file?file_path=cuba-cli%2F1.0.1%2Fcuba-cli-1.0.1-linux.zip",
         platform = Linux64
       ),
       Version(
-        candidate = "cubacli",
+        candidate = "cuba-cli",
         version = "1.0.1",
         url = "https://bintray.com/cuba-platform/tools/download_file?file_path=cuba-cli%2F1.0.1%2Fcuba-cli-1.0.1-macos.zip",
         platform = MacOSX
       ),
       Version(
-        candidate = "cubacli",
+        candidate = "cuba-cli",
         version = "1.0.1",
         url = "https://bintray.com/cuba-platform/tools/download_file?file_path=cuba-cli%2F1.0.1%2Fcuba-cli-1.0.1-windows.zip",
         platform = Windows
