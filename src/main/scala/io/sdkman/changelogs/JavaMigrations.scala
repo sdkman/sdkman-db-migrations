00b40c6f742bdc935b81a93e1b90d07064a9d1e0
Szymon Stepniak <szymon.stepniak@gmail.com>
2019-01-15 13:11:58 +0100

    Added GraalVM 1.0.0-RC11

--- src/main/scala/io/sdkman/changelogs/JavaMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/JavaMigrations.scala
@@ -343,5 +343,22 @@ class JavaMigrations {
       .insert()
   }
 
+  @ChangeSet(order = "069", id = "069-add_graalvm_1_0_0_rc_11", author = "wololock")
+  def migrate069(implicit db: MongoDatabase) = {
+    List(
+      Version(
+        candidate = "java",
+        version = "1.0.0-rc-11-grl",
+        url = "https://github.com/oracle/graal/releases/download/vm-1.0.0-rc11/graalvm-ce-1.0.0-rc11-linux-amd64.tar.gz",
+        platform = Linux64),
+      Version(
+        candidate = "java",
+        version = "1.0.0-rc-11-grl",
+        url = "https://github.com/oracle/graal/releases/download/vm-1.0.0-rc11/graalvm-ce-1.0.0-rc11-macos-amd64.tar.gz",
+        platform = MacOSX))
+      .validate()
+      .insert()
+  }
+
 
 }
