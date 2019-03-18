391c6124e7ea0ee8cf412342a433ea1eff04fe53
Szymon Stepniak <szymon.stepniak@gmail.com>
2019-03-18 21:37:47 +0100

    added GraalVM 1.0.0-RC14 CE

--- src/main/scala/io/sdkman/changelogs/JavaMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/JavaMigrations.scala
@@ -534,4 +534,21 @@ class JavaMigrations {
       .insert()
     Seq(Linux64, MacOSX, Windows).foreach(removeVersion("java", "13.ea.07-open", _))
   }
+
+  @ChangeSet(order = "095", id = "095-add_graalvm_1_0_0_rc_14", author = "wololock")
+  def migrate095(implicit db: MongoDatabase) = {
+    List(
+      Version(
+        candidate = "java",
+        version = "1.0.0-rc-14-grl",
+        url = "https://github.com/oracle/graal/releases/download/vm-1.0.0-rc14/graalvm-ce-1.0.0-rc14-linux-amd64.tar.gz",
+        platform = Linux64),
+      Version(
+        candidate = "java",
+        version = "1.0.0-rc-14-grl",
+        url = "https://github.com/oracle/graal/releases/download/vm-1.0.0-rc14/graalvm-ce-1.0.0-rc14-macos-amd64.tar.gz",
+        platform = MacOSX))
+      .validate()
+      .insert()
+  }
 }
