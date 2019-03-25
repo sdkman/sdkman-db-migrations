4baafacf98f6b475294abd9e809bdb66b617d94b
Eddú Meléndez <eddu.melendez@gmail.com>
2019-03-25 17:17:08 -0400

    Add Java 13ea13

--- src/main/scala/io/sdkman/changelogs/JavaMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/JavaMigrations.scala
@@ -582,4 +582,14 @@ class JavaMigrations {
     ).validate().insert()
   }
 
+  @ChangeSet(order = "099", id = "099-add_openjdk_java_13-ea-13", author = "eddumelendez")
+  def migrate097(implicit db: MongoDatabase): Unit = {
+    List(
+      Version("java", "13.ea.13-open", "https://download.java.net/java/early_access/jdk13/12/GPL/openjdk-13-ea+13_linux-x64_bin.tar.gz", Linux64),
+      Version("java", "13.ea.13-open", "https://download.java.net/java/early_access/jdk13/12/GPL/openjdk-13-ea+13_osx-x64_bin.tar.gz", MacOSX),
+      Version("java", "13.ea.13-open", "https://download.java.net/java/early_access/jdk13/12/GPL/openjdk-13-ea+13_windows-x64_bin.zip", Windows))
+      .validate()
+      .insert()
+    Seq(Linux64, MacOSX, Windows).foreach(removeVersion("java", "13.ea.12-open", _))
+  }
 }
