18593e876e7d03d1413609c009760c55e2f608f7
Szymon Stepniak <szymon.stepniak@gmail.com>
2019-04-24 10:08:34 +0200

    Remove old GraalVM CE from 1.0.0-RC8 to 1.0.0-RC12

--- src/main/scala/io/sdkman/changelogs/JavaMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/JavaMigrations.scala
@@ -826,6 +826,10 @@ class JavaMigrations {
 
   @ChangeSet(order = "126", id = "126-add_graalvm_1_0_0_rc_16", author = "wololock")
   def migrate126(implicit db: MongoDatabase) = {
+    Range(8,13).foreach(v => {
+      removeVersion(candidate = "java", version = s"1.0.0-rc-$v-grl", platform = Linux64)
+      removeVersion(candidate = "java", version = s"1.0.0-rc-$v-grl", platform = MacOSX)
+    })
     List(
       Version(
         candidate = "java",
