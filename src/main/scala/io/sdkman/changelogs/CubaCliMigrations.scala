0d25c2593d23a397539e3746e520cfe5c8c311de
Aleksei Nakhimov <nahimov@haulmont.com>
2019-05-17 12:03:16 +0400

    Change CUBA CLI download URLs due to migration of packages from Bintray to KeyCDN

--- src/main/scala/io/sdkman/changelogs/CubaCliMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/CubaCliMigrations.scala
@@ -38,4 +38,26 @@ class CubaCliMigrations {
       .validate()
       .insert()
   }
+
+  @ChangeSet(order = "002", id = "002_change_versions_urls", author = "torquemada163")
+  def migration002(implicit db: MongoDatabase) =
+    Seq("1.0.1", "1.0.2", "1.0.3", "2.0.1", "2.0.2", "2.0.3", "2.0.4", "2.1.0").foreach { version =>
+
+      removeVersion("spark", version)
+      List(
+        Version(
+          candidate = "cuba",
+          version = version,
+          url = s"https://cdn.cuba-platform.com/cuba-cli/$version/cuba-cli-$version-linux.zip",
+          platform = Linux64
+        ),
+        Version(
+          candidate = "cuba",
+          version = version,
+          url = s"https://cdn.cuba-platform.com/cuba-cli/$version/cuba-cli-$version-macos.zip",
+          platform = MacOSX
+        ))
+        .validate()
+        .insert()
+    }
 }
