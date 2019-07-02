bcaed030aaf837f90c98d55a3b927098b08a3e5a
Hector Miuler Malpica Gallegos <miuler@gmail.com>
2019-07-02 03:22:24 -0500

    upgrade java version

--- src/main/scala/io/sdkman/changelogs/JavaMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/JavaMigrations.scala
@@ -295,14 +295,14 @@ class JavaMigrations {
     Seq(Linux64, MacOSX).foreach(platform => removeVersion(candidate = "java", version = "1.0.0-rc-14-grl", platform))
   }
 
-  @ChangeSet(order = "143", id = "143-add_openjdk_java_13-ea-24", author = "eddumelendez")
-  def migrate143(implicit db: MongoDatabase): Unit = {
+  @ChangeSet(order = "144", id = "144-add_openjdk_java_13-ea-25", author = "Miuler")
+  def migrate144(implicit db: MongoDatabase): Unit = {
     List(
-      Version("java", "13.ea.24-open", "https://download.java.net/java/early_access/jdk13/24/GPL/openjdk-13-ea+24_linux-x64_bin.tar.gz", Linux64, Some(OpenJDK)),
-      Version("java", "13.ea.24-open", "https://download.java.net/java/early_access/jdk13/24/GPL/openjdk-13-ea+24_osx-x64_bin.tar.gz", MacOSX, Some(OpenJDK)),
-      Version("java", "13.ea.24-open", "https://download.java.net/java/early_access/jdk13/24/GPL/openjdk-13-ea+24_windows-x64_bin.zip", Windows, Some(OpenJDK)))
+      Version("java", "13.ea.25-open", "https://download.java.net/java/early_access/jdk13/25/GPL/openjdk-13-ea+25_linux-x64_bin.tar.gz", Linux64, Some(OpenJDK)),
+      Version("java", "13.ea.25-open", "https://download.java.net/java/early_access/jdk13/25/GPL/openjdk-13-ea+25_osx-x64_bin.tar.gz", MacOSX, Some(OpenJDK)),
+      Version("java", "13.ea.25-open", "https://download.java.net/java/early_access/jdk13/25/GPL/openjdk-13-ea+25_windows-x64_bin.zip", Windows, Some(OpenJDK)))
       .validate()
       .insert()
-    Seq(Linux64, MacOSX, Windows).foreach(removeVersion("java", "13.ea.23-open", _))
+    Seq(Linux64, MacOSX, Windows).foreach(removeVersion("java", "13.ea.24-open", _))
   }
 }
