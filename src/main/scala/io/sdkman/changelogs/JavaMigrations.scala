b9650dac863909afe040bd7be67d6c43829323bc
Philip Lourandos <philip.lourandos@gmail.com>
2019-10-22 14:31:18 +0200

    * Corrected version for removal

--- src/main/scala/io/sdkman/changelogs/JavaMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/JavaMigrations.scala
@@ -719,6 +719,6 @@ class JavaMigrations {
       Version("java", "11.0.5-amzn", "https://d3pxv6yz143wms.cloudfront.net/11.0.5.10.1/amazon-corretto-11.0.5.10.1-macosx-x64.tar.gz", MacOSX, Some(Amazon)),
       Version("java", "11.0.5-amzn", "https://d3pxv6yz143wms.cloudfront.net/11.0.5.10.1/amazon-corretto-11.0.5.10.1-windows-x64.zip", Windows, Some(Amazon))
     )
-    Seq(Linux64, MacOSX, Windows).foreach(platform => removeVersion(candidate = "java", version = "1.0.4-amzn", platform))
+    Seq(Linux64, MacOSX, Windows).foreach(platform => removeVersion(candidate = "java", version = "11.0.4-amzn", platform))
   }
 }
