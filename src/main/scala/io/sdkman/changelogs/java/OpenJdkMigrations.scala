dfaf4353dec5ee2e012bad246f431e08246a4f0d
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2019-11-08 02:23:02 -0500

    Add OpenJDK 14ea22 (#177)

--- src/main/scala/io/sdkman/changelogs/java/OpenJdkMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/java/OpenJdkMigrations.scala
@@ -18,4 +18,15 @@ class OpenJdkMigrations {
     Seq(Linux64, MacOSX, Windows).foreach(removeVersion("java", "14.ea.18-open", _))
   }
 
+  @ChangeSet(order = "002", id = "002-add_openjdk_java_14-ea-22", author = "eddumelendez")
+  def migrate002(implicit db: MongoDatabase): Unit = {
+    List(
+      Version("java", "14.ea.22-open", "https://download.java.net/java/early_access/jdk14/22/GPL/openjdk-14-ea+22_linux-x64_bin.tar.gz", Linux64, Some(OpenJDK)),
+      Version("java", "14.ea.22-open", "https://download.java.net/java/early_access/jdk14/22/GPL/openjdk-14-ea+22_osx-x64_bin.tar.gz", MacOSX, Some(OpenJDK)),
+      Version("java", "14.ea.22-open", "https://download.java.net/java/early_access/jdk14/22/GPL/openjdk-14-ea+22_windows-x64_bin.zip", Windows, Some(OpenJDK)))
+      .validate()
+      .insert()
+    Seq(Linux64, MacOSX, Windows).foreach(removeVersion("java", "14.ea.20-open", _))
+  }
+
 }
