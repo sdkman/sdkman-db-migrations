a8b6c6a193c30c28c8f8163ba2438b417cd92672
Leonardo Lucena <leonardo.lucena@ifrn.edu.br>
2019-12-04 12:47:31 -0500

    add graalvm 19.3.0

--- src/main/scala/io/sdkman/changelogs/java/GraalVmMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/java/GraalVmMigrations.scala
@@ -49,4 +49,28 @@ class GraalVmMigrations {
       .validate()
       .insert()
 
+  @ChangeSet(order = "002", id = "002-add_graalvm_19_3_0", author = "lrlucena")
+  def migrate002(implicit db: MongoDatabase) =
+    List(
+      Version(
+        candidate = "java",
+        version = "19.3.0-grl",
+        url = "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-19.3.0/graalvm-ce-java11-linux-amd64-19.3.0.tar.gz",
+        platform = Linux64,
+        vendor = Some(Graal)),
+      Version(
+        candidate = "java",
+        version = "19.3.0-grl",
+        url = "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-19.3.0/graalvm-ce-java11-darwin-amd64-19.3.0.tar.gz",
+        platform = MacOSX,
+        vendor = Some(Graal)),
+      Version(
+        candidate = "java",
+        version = "19.3.0-grl",
+        url = "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-19.3.0/graalvm-ce-java11-windows-amd64-19.3.0.zip",
+        platform = Windows,
+        vendor = Some(Graal)))
+      .validate()
+      .insert()
+    Seq(Linux64, MacOSX, Windows).foreach(removeVersion("java", "19.3.0.r11-grl", _))
 }
