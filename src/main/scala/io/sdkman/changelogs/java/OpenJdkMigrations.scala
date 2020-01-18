01e4f2091393cea47630c410086c40d6bf13001c
Kenji Saito <ken-yo@mbr.nifty.com>
2020-01-18 18:13:30 +0900

    Fix the migrate error
    Delete unavailable "OpenJDK 14 EA"

--- src/main/scala/io/sdkman/changelogs/java/OpenJdkMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/java/OpenJdkMigrations.scala
@@ -7,17 +7,6 @@ import io.sdkman.changelogs.{Linux64, MacOSX, OpenJDK, Version, Windows, removeV
 @ChangeLog(order = "022")
 class OpenJdkMigrations {
 
-  @ChangeSet(order = "001", id = "001-add_openjdk_java_14-ea-20", author = "eddumelendez")
-  def migrate001(implicit db: MongoDatabase): Unit = {
-    List(
-      Version("java", "14.ea.20-open", "https://download.java.net/java/early_access/jdk14/20/GPL/openjdk-14-ea+20_linux-x64_bin.tar.gz", Linux64, Some(OpenJDK)),
-      Version("java", "14.ea.20-open", "https://download.java.net/java/early_access/jdk14/20/GPL/openjdk-14-ea+20_osx-x64_bin.tar.gz", MacOSX, Some(OpenJDK)),
-      Version("java", "14.ea.20-open", "https://download.java.net/java/early_access/jdk14/20/GPL/openjdk-14-ea+20_windows-x64_bin.zip", Windows, Some(OpenJDK)))
-      .validate()
-      .insert()
-    Seq(Linux64, MacOSX, Windows).foreach(removeVersion("java", "14.ea.18-open", _))
-  }
-
   @ChangeSet(order = "002", id = "002-add_openjdk_java_14-ea-22", author = "eddumelendez")
   def migrate002(implicit db: MongoDatabase): Unit = {
     List(
