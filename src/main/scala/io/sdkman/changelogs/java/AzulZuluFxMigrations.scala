547359c625facc8f06f20bea35267da1f83da45e
Kenji Saito <ken-yo@mbr.nifty.com>
2020-01-27 22:57:54 +0900

    [Review fix] Fix the same wrong

--- src/main/scala/io/sdkman/changelogs/java/AzulZuluFxMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/java/AzulZuluFxMigrations.scala
@@ -9,14 +9,10 @@ class AzulZuluFxMigrations {
 
   @ChangeSet(
     order = "0001",
-    id = "0001-add_zulufx_8_0_242",
-    author = "dimitryc"
+    id = "0001-add_zulufx_8_0_232",
+    author = "poad"
   )
   def migrate0001(implicit db: MongoDatabase) = {
-    Seq(Linux64, Windows, MacOSX).foreach(
-      platform =>
-        removeVersion(candidate = "java", version = "8.0.202-zulu", platform)
-    )
     List(
       Version(
         "java",
@@ -42,36 +38,53 @@ class AzulZuluFxMigrations {
     ).validate().insert()
   }
 
-  @ChangeSet(order = "0002", id = "0002-add_zulufx_11_0_6", author = "dimitryc")
+  @ChangeSet(order = "0002", id = "0002-add_zulufx_11_0_5", author = "poad")
   def migrate0002(implicit db: MongoDatabase) = {
-    Seq(Linux64, Windows, MacOSX).foreach(
-      platform =>
-        removeVersion(candidate = "java", version = "11.0.2-zulu", platform)
-    )
     List(
       Version(
         "java",
-        "11_0_6-zulufx",
+        "11.0.5-zulufx",
         "https://cdn.azul.com/zulu/bin/zulu11.35.15-ca-fx-jdk11.0.5-linux_x64.tar.gz",
         Linux64,
         Some(Zulu)
       ),
       Version(
         "java",
-        "11_0_6-zulufx",
+        "11.0.5-zulufx",
         "https://cdn.azul.com/zulu/bin/zulu11.35.15-ca-fx-jdk11.0.5-win_x64.zip",
         Windows,
         Some(Zulu)
       ),
       Version(
         "java",
-        "11_0_6-zulufx",
+        "11.0.5-zulufx",
         "https://cdn.azul.com/zulu/bin/zulu11.35.15-ca-fx-jdk11.0.5-macosx_x64.tar.gz",
         MacOSX,
         Some(Zulu)
       )
     ).validate().insert()
-    setCandidateDefault("java", "11_0_6-zulufx")
   }
+
+  @ChangeSet(
+    order = "0003",
+    id = "0003-remove_old-zulufx-8.0.202",
+    author = "poad"
+  )
+  def migrate0003(implicit db: MongoDatabase) =
+    Seq(Linux64, Windows, MacOSX).foreach(
+      platform =>
+        removeVersion(candidate = "java", version = "8.0.202-zulufx", platform)
+    )
+
+  @ChangeSet(
+    order = "0004",
+    id = "0004-remove_old-zulufx11.0.2",
+    author = "poad"
+  )
+  def migrate0004(implicit db: MongoDatabase) =
+    Seq(Linux64, Windows, MacOSX).foreach(
+      platform =>
+        removeVersion(candidate = "java", version = "11.0.2-zulufx", platform)
+    )
 
 }
