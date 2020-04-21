8148f37da5d726bab4cb3596b82e4759534d12fc
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2020-04-21 10:06:26 +0200

    Just add a new candidate

--- src/main/scala/io/sdkman/changelogs/BTraceMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/BTraceMigrations.scala
@@ -5,44 +5,18 @@ import com.mongodb.client.MongoDatabase
 
 @ChangeLog(order = "027")
 class BTraceMigrations {
-
-  @ChangeSet(order = "001", id = "001-add_btrace_2_0_1", author = "bachorikj")
+  @ChangeSet(
+    order = "001",
+    id = "001_add_btrace_candidate",
+    author = "bachorikj"
+  )
   def migration001(implicit db: MongoDatabase) = {
-    List(
-      Version(
-        "btrace",
-        "2.0.1",
-        "https://github.com/btraceio/btrace/releases/download/v2.0.1/btrace-bin.tar.gz",
-        Universal
-      ),
-      Version(
-        "btrace",
-        "2.0.1",
-        "https://github.com/btraceio/btrace/releases/download/v2.0.1/btrace-bin.zip",
-        Windows
-      )
-    ).validate()
-      .insert()
-
-    setCandidateDefault("btrace", "2.0.1")
-  }
-
-  @ChangeSet(order = "002", id = "002-add_btrace_1_3_11_3", author = "bachorik")
-  def migration002(implicit db: MongoDatabase) = {
-    List(
-      Version(
-        "btrace",
-        "1.3.11.3",
-        "https://github.com/btraceio/btrace/releases/download/v1.3.11.3/btrace-bin-1.3.11.3.tgz",
-        Universal
-      ),
-      Version(
-        "btrace",
-        "1.3.11.3",
-        "https://github.com/btraceio/btrace/releases/download/v1.3.11.3/btrace-bin-1.3.11.3.zip",
-        Windows
-      )
-    ).validate()
-      .insert()
+    Candidate(
+      candidate = "btrace",
+      name = "BTrace",
+      description =
+        "BTrace - a safe, dynamic tracing tool for the Java platform",
+      websiteUrl = "https://github.com/btraceio/btrace"
+    ).insert()
   }
 }
