a3ae89e32430e211b9da28bba4ac820c53385ef8
Manik Magar <manik.magar@gmail.com>
2020-04-26 09:39:34 -0400

    Update candidate name to be less verbose

--- src/main/scala/io/sdkman/changelogs/MuleFlowDiagramsMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/MuleFlowDiagramsMigrations.scala
@@ -3,7 +3,7 @@ package io.sdkman.changelogs
 import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
 import com.mongodb.client.MongoDatabase
 
-@ChangeLog(order = "027")
+@ChangeLog(order = "028")
 class MuleFlowDiagramsMigrations {
 
   @ChangeSet(
@@ -13,7 +13,7 @@ class MuleFlowDiagramsMigrations {
   )
   def migration001(implicit db: MongoDatabase): Candidate = {
     Candidate(
-      candidate = "muleflowdiagrams",
+      candidate = "mulefd",
       name = "Mule Flow Diagrams",
       description =
         "Mule Flow Diagrams is an open source tool that lets you generate flow dependency graph and diagrams for your Mule configurations.",
