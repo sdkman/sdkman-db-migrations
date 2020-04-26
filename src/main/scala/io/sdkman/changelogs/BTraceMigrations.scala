e1fac9623fc949f574a697ffad51818b0975587d
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-04-26 12:03:41 +0100

    Remove quoted characters from BTrace description.

--- src/main/scala/io/sdkman/changelogs/BTraceMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/BTraceMigrations.scala
@@ -17,7 +17,7 @@ class BTraceMigrations {
       description =
         "BTrace is a safe, dynamic tracing tool for the Java platform. BTrace can be used to dynamically trace a " +
           "running Java program (similar to DTrace for OpenSolaris applications and OS). BTrace dynamically " +
-          "instruments the classes of the target application to inject tracing code (\"bytecode tracing\").",
+          "instruments the classes of the target application to inject bytecode tracing code.",
       websiteUrl = "https://github.com/btraceio/btrace"
     ).insert()
   }
