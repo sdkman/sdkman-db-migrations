249b67200eb835a6cf65a85c366f6b9bb8d82703
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-05-09 11:51:41 +0100

    Add 2.0.0.M3 platform specific binaries for micronaut.

--- src/main/scala/io/sdkman/changelogs/MicronautMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/MicronautMigrations.scala
@@ -20,4 +20,34 @@ class MicronautMigrations {
       websiteUrl = "http://micronaut.io/"
     ).insert()
   }
+
+  @ChangeSet(
+    order = "002",
+    id = "002_replace_2_0_0_M3_with_platform_binaries",
+    author = "marc0der"
+  )
+  def migration002(implicit db: MongoDatabase) = {
+    removeVersion("micronaut", "2.0.0.M3", Universal)
+    List(
+      Version(
+        "micronaut",
+        "2.0.0.M3",
+        "https://github.com/micronaut-projects/micronaut-starter/releases/download/v2.0.0.M3/mn-darwin-amd64-v2.0.0.M3.tar.gz",
+        MacOSX
+      ),
+      Version(
+        "micronaut",
+        "2.0.0.M3",
+        "https://github.com/micronaut-projects/micronaut-starter/releases/download/v2.0.0.M3/mn-linux-amd64-v2.0.0.M3.tar.gz",
+        Linux64
+      ),
+      Version(
+        "micronaut",
+        "2.0.0.M3",
+        "https://github.com/micronaut-projects/micronaut-starter/releases/download/v2.0.0.M3/mn-win-amd64-v2.0.0.M3.zip",
+        Windows
+      )
+    ).validate()
+      .insert()
+  }
 }
