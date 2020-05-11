f0cb26fc8a0ddce3be43bf87122def41567d358c
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-05-11 07:55:52 +0100

    Update micronaut 2.0.0.M3 to use zips.

--- src/main/scala/io/sdkman/changelogs/MicronautMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/MicronautMigrations.scala
@@ -50,4 +50,28 @@ class MicronautMigrations {
     ).validate()
       .insert()
   }
+
+  @ChangeSet(
+    order = "003",
+    id = "002_replace_2_0_0_M3_with_platform_zip_binaries",
+    author = "marc0der"
+  )
+  def migration003(implicit db: MongoDatabase) = {
+    Seq(Linux64, MacOSX).foreach(removeVersion("micronaut", "2.0.0.M3", _))
+    List(
+      Version(
+        "micronaut",
+        "2.0.0.M3",
+        "https://github.com/micronaut-projects/micronaut-starter/releases/download/v2.0.0.M3/mn-darwin-amd64-v2.0.0.M3.zip",
+        MacOSX
+      ),
+      Version(
+        "micronaut",
+        "2.0.0.M3",
+        "https://github.com/micronaut-projects/micronaut-starter/releases/download/v2.0.0.M3/mn-linux-amd64-v2.0.0.M3.zip",
+        Linux64
+      )
+    ).validate()
+      .insert()
+  }
 }
