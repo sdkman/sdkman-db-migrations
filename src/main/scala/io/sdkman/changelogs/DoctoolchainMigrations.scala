6fac967d08f927d049006af7aca5bd9eb3acdc92
Ralf D. Müller <ralf.d.mueller@gmail.com>
2020-06-17 17:22:29 +0200

    fixed order id

--- src/main/scala/io/sdkman/changelogs/DoctoolchainMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/DoctoolchainMigrations.scala
@@ -3,7 +3,7 @@ package io.sdkman.changelogs
 import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
 import com.mongodb.client.MongoDatabase
 
-@ChangeLog(order = "015")
+@ChangeLog(order = "030")
 class DoctoolchainMigrations {
 
   @ChangeSet(
