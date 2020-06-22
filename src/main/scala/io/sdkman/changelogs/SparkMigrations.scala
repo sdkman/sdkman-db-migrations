08f7a81c568ad90c3811dcb9003bb0967d69f156
flvndh <17010377+flvndh@users.noreply.github.com>
2020-06-22 10:16:26 +0200

    Add Apache Spark 2.4.6

--- src/main/scala/io/sdkman/changelogs/SparkMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/SparkMigrations.scala
@@ -115,6 +115,20 @@ class SparkMigrations {
       "https://archive.apache.org/dist/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz"
     ).validate()
       .insert()
+  }
+
+  @ChangeSet(
+    order = "010",
+    id = "010-add_spark_2.4.6",
+    author = "flvndh"
+  )
+  def migration010(implicit db: MongoDatabase) = {
+    Version(
+      "spark",
+      "2.4.6",
+      "https://archive.apache.org/dist/spark/spark-2.4.6/spark-2.4.6-bin-hadoop2.7.tgz"
+    ).validate()
+      .insert()
       .asCandidateDefault()
   }
 }
