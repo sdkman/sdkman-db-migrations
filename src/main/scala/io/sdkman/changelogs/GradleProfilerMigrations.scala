2aa2d82ed87c781c866152c3a77a7183feefe208
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-08-04 15:24:54 +0100

    Format Gradle Profiler migration.

--- src/main/scala/io/sdkman/changelogs/GradleProfilerMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/GradleProfilerMigrations.scala
@@ -5,21 +5,26 @@ import com.mongodb.client.MongoDatabase
 
 @ChangeLog(order = "033")
 class GradleProfilerMigrations {
-    @ChangeSet(order = "001", id = "001-add_gradleprofiler_0.12.0", author = "wolfs")
-    def migration001(implicit db: MongoDatabase): Unit = {
-      Candidate(
-        candidate = "gradleprofiler",
-        name = "Gradle profiler",
-        description = "A tool for gathering profiling and benchmarking information for Gradle builds",
-        websiteUrl = "https://github.com/gradle/gradle-profiler"
-      ).insert()
+  @ChangeSet(
+    order = "001",
+    id = "001-add_gradleprofiler_0.12.0",
+    author = "wolfs"
+  )
+  def migration001(implicit db: MongoDatabase): Unit = {
+    Candidate(
+      candidate = "gradleprofiler",
+      name = "Gradle profiler",
+      description =
+        "A tool for gathering profiling and benchmarking information for Gradle builds",
+      websiteUrl = "https://github.com/gradle/gradle-profiler"
+    ).insert()
 
-      Version(
-        "gradleprofiler",
-        "0.12.0",
-        "https://repo.gradle.org/gradle/ext-releases-local/org/gradle/profiler/gradle-profiler/0.12.0/gradle-profiler-0.12.0.zip"
-      ).validate()
-        .insert()
-        .asCandidateDefault()
-    }
+    Version(
+      "gradleprofiler",
+      "0.12.0",
+      "https://repo.gradle.org/gradle/ext-releases-local/org/gradle/profiler/gradle-profiler/0.12.0/gradle-profiler-0.12.0.zip"
+    ).validate()
+      .insert()
+      .asCandidateDefault()
+  }
 }
