87bdee1b809845322dcdd689271fcf890add2694
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2020-09-23 04:52:12 +0200

    shorter ballerina version, separate change sets

--- src/main/scala/io/sdkman/changelogs/BallerinaMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/BallerinaMigrations.scala
@@ -21,10 +21,17 @@ class BallerinaMigrations {
         "Open source programming language and platform for cloud-era application programmers to easily write software that just works.",
       websiteUrl = "https://ballerina.io/"
     ).insert()
+  }
 
+  @ChangeSet(
+    order = "002",
+    id = "002_add_ballerina_swan_lake_p3",
+    author = "bassmake"
+  )
+  def migration002(implicit db: MongoDatabase): Unit = {
     Version(
       CandidateName,
-      "swan-lake-preview3",
+      "swan-lake-p3",
       "https://dist.ballerina.io/downloads/swan-lake-preview3/ballerina-swan-lake-preview3.zip"
     ).validate()
       .insert()
