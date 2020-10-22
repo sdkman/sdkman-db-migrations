4804a038001bce2514bb281263428248fa5e2144
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2020-10-22 10:39:39 -0500

    Add Corretto 11.0.9.11.1 (#375)

--- src/main/scala/io/sdkman/changelogs/java/AmazonCorrettoMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/java/AmazonCorrettoMigrations.scala
@@ -317,6 +317,34 @@ class AmazonCorrettoMigrations {
       .insert()
 
   @ChangeSet(
+    order = "0012",
+    id = "0012-add_corretto_java11_update_9",
+    author = "eddumelendez"
+  )
+  def migrate0012(implicit db: MongoDatabase) =
+    Map(
+      LinuxARM64 -> ("11.0.9.11.1", "linux-aarch64.tar.gz"),
+      Linux64    -> ("11.0.9.11.1", "linux-x64.tar.gz"),
+      MacOSX     -> ("11.0.9.11.1", "macosx-x64.tar.gz"),
+      Windows    -> ("11.0.9.11.1", "windows-x64-jdk.zip")
+    ).map {
+        case (platform, (version, suffix)) =>
+          Version(
+            "java",
+            "11.0.9-amzn",
+            s"https://corretto.aws/downloads/resources/$version/amazon-corretto-$version-$suffix",
+            platform,
+            Some(Amazon)
+          )
+      }
+      .toList
+      .validate()
+      .insert()
+      .foreach { version =>
+        removeVersion("java", "11.0.8-amzn", version.platform)
+      }
+
+  @ChangeSet(
     order = "0013",
     id = "0013-add_corretto_java8_update_272",
     author = "eddumelendez"
