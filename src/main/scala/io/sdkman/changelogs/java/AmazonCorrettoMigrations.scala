996c769514979c613aa96cc548e19fe1a90833f7
Eddú Meléndez <eddu.melendez@gmail.com>
2020-10-23 19:52:14 -0500

    Add Corretto 11.0.9.11.1-1

--- src/main/scala/io/sdkman/changelogs/java/AmazonCorrettoMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/java/AmazonCorrettoMigrations.scala
@@ -33,34 +33,6 @@ class AmazonCorrettoMigrations {
       .insert()
 
   @ChangeSet(
-    order = "0012",
-    id = "0012-add_corretto_java11_update_9",
-    author = "eddumelendez"
-  )
-  def migrate0012(implicit db: MongoDatabase) =
-    Map(
-      LinuxARM64 -> ("11.0.9.11.1", "linux-aarch64.tar.gz"),
-      Linux64    -> ("11.0.9.11.1", "linux-x64.tar.gz"),
-      MacOSX     -> ("11.0.9.11.1", "macosx-x64.tar.gz"),
-      Windows    -> ("11.0.9.11.1", "windows-x64-jdk.zip")
-    ).map {
-        case (platform, (version, suffix)) =>
-          Version(
-            "java",
-            "11.0.9-amzn",
-            s"https://corretto.aws/downloads/resources/$version/amazon-corretto-$version-$suffix",
-            platform,
-            Some(Amazon)
-          )
-      }
-      .toList
-      .validate()
-      .insert()
-      .foreach { version =>
-        removeVersion("java", "11.0.8-amzn", version.platform)
-      }
-
-  @ChangeSet(
     order = "0014",
     id = "0014-add_corretto_java11_update_9",
     author = "eddumelendez"
@@ -117,6 +89,42 @@ class AmazonCorrettoMigrations {
             "java",
             "8.0.272-amzn",
             s"https://corretto.aws/downloads/resources/$version/amazon-corretto-$version-$suffix",
+            platform,
+            Some(Amazon)
+          )
+      }
+      .toList
+      .validate()
+      .insert()
+
+  @ChangeSet(
+    order = "0017",
+    id = "0017-remove_corretto_11.0.9.11.1",
+    author = "eddumelendez"
+  )
+  def migrate0017(implicit db: MongoDatabase) =
+    Seq(
+      LinuxARM64,
+      Linux64
+    ).foreach { platform =>
+      removeVersion("java", "11.0.9-amzn", platform)
+    }
+
+  @ChangeSet(
+    order = "0018",
+    id = "0018-add_corretto_11.0.9.11.1-1",
+    author = "eddumelendez"
+  )
+  def migrate0018(implicit db: MongoDatabase) =
+    Map(
+      LinuxARM64 -> "linux-aarch64.tar.gz",
+      Linux64    -> "linux-x64.tar.gz"
+    ).map {
+        case (platform, suffix) =>
+          Version(
+            "java",
+            "11.0.9-amzn",
+            s"https://corretto.aws/downloads/resources/11.0.9.11.1-1/amazon-corretto-11.0.9.11.1-$suffix",
             platform,
             Some(Amazon)
           )
