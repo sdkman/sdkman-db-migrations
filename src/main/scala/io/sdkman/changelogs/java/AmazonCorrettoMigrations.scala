720d8eec60bee13453904171e899e9a07badfb3f
Eddú Meléndez <eddu.melendez@gmail.com>
2020-10-23 21:53:00 -0500

    Add Corretto 15.0.1.9.1

--- src/main/scala/io/sdkman/changelogs/java/AmazonCorrettoMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/java/AmazonCorrettoMigrations.scala
@@ -8,31 +8,6 @@ import io.sdkman.changelogs.{Amazon, Linux64, MacOSX, Version, Windows, _}
 class AmazonCorrettoMigrations {
 
   @ChangeSet(
-    order = "0011",
-    id = "0011-add_corretto_java15.0.0",
-    author = "poad"
-  )
-  def migrate0011(implicit db: MongoDatabase) =
-    Map(
-      LinuxARM64 -> ("15.0.0.36.1", "linux-aarch64.tar.gz"),
-      Linux64    -> ("15.0.0.36.1", "linux-x64.tar.gz"),
-      MacOSX     -> ("15.0.0.36.1", "macosx-x64.tar.gz"),
-      Windows    -> ("15.0.0.36.1", "windows-x64-jdk.zip")
-    ).map {
-        case (platform, (version, suffix)) =>
-          Version(
-            "java",
-            "15.0.0-amzn",
-            s"https://corretto.aws/downloads/resources/$version/amazon-corretto-$version-$suffix",
-            platform,
-            Some(Amazon)
-          )
-      }
-      .toList
-      .validate()
-      .insert()
-
-  @ChangeSet(
     order = "0014",
     id = "0014-add_corretto_java11_update_9",
     author = "eddumelendez"
@@ -133,4 +108,31 @@ class AmazonCorrettoMigrations {
       .validate()
       .insert()
 
+  @ChangeSet(
+    order = "0019",
+    id = "0019-add_corretto_15.0.1.9.1",
+    author = "eddumelendez"
+  )
+  def migrate0019(implicit db: MongoDatabase) =
+    Map(
+      LinuxARM64 -> "linux-aarch64.tar.gz",
+      Linux64    -> "linux-x64.tar.gz",
+      MacOSX     -> "macosx-x64.tar.gz",
+      Windows    -> "windows-x64-jdk.zip"
+    ).map {
+        case (platform, suffix) =>
+          Version(
+            "java",
+            "15.0.1-amzn",
+            s"https://corretto.aws/downloads/resources/15.0.1.9.1/amazon-corretto-15.0.1.9.1-$suffix",
+            platform,
+            Some(Amazon)
+          )
+      }
+      .toList
+      .validate()
+      .insert()
+      .foreach { version =>
+        removeVersion("java", "15.0.0-amzn", version.platform)
+      }
 }
