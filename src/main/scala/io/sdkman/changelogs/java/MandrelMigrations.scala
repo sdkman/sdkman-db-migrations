106b32932698201131ca14db711992ca16573d65
Kenji Saito <ken-yo@mbr.nifty.com>
2020-11-10 08:22:52 +0900

    Update Mandrel GraalVM to 20.2.0.0.Final

--- src/main/scala/io/sdkman/changelogs/java/MandrelMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/java/MandrelMigrations.scala
@@ -2,7 +2,7 @@ package io.sdkman.changelogs.java
 
 import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
 import com.mongodb.client.MongoDatabase
-import io.sdkman.changelogs.{Mandrel, Linux64, Version}
+import io.sdkman.changelogs.{Linux64, Mandrel, Version}
 
 @ChangeLog(order = "035")
 class MandrelMigrations {
@@ -19,6 +19,24 @@ class MandrelMigrations {
         version = "20.1.0.1-mandrel",
         url =
           "https://github.com/graalvm/mandrel/releases/download/mandrel-20.1.0.1.Final/mandrel-java11-linux-amd64-20.1.0.1.Final.tar.gz",
+        platform = Linux64,
+        vendor = Some(Mandrel)
+      )
+    ).validate()
+      .insert()
+
+  @ChangeSet(
+    order = "002",
+    id = "002-add_mandrel_20_2_0_0",
+    author = "poad"
+  )
+  def migrate002(implicit db: MongoDatabase): Unit =
+    List(
+      Version(
+        candidate = "java",
+        version = "20.2.0.0-mandrel",
+        url =
+          "https://github.com/graalvm/mandrel/releases/download/mandrel-20.2.0.0.Final/mandrel-java11-linux-amd64-20.2.0.0.Final.tar.gz",
         platform = Linux64,
         vendor = Some(Mandrel)
       )
