36d47b603fd2f61cd3eb9e36404324d822fd3cdf
Eddú Meléndez <eddu.melendez@gmail.com>
2020-11-13 09:44:15 -0600

    Add Alibaba Dragonwell 8u272

--- src/main/scala/io/sdkman/changelogs/java/AlibabaDragonwellMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/java/AlibabaDragonwellMigrations.scala
@@ -2,32 +2,10 @@ package io.sdkman.changelogs.java
 
 import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
 import com.mongodb.client.MongoDatabase
-import io.sdkman.changelogs.{Alibaba, Linux64, Version}
+import io.sdkman.changelogs.{Alibaba, Linux64, Version, removeVersion}
 
 @ChangeLog(order = "039")
 class AlibabaDragonwellMigrations {
-
-  @ChangeSet(
-    order = "001",
-    id = "001-add_dragonwell-8u262",
-    author = "eddumelendez"
-  )
-  def migrate001(implicit db: MongoDatabase): Unit =
-    Map(
-      Linux64 -> "Alibaba_Dragonwell_8.4.4-GA_Linux_x64.tar.gz"
-    ).map {
-        case (platform, binary) =>
-          Version(
-            "java",
-            "8u262-albba",
-            s"https://github.com/alibaba/dragonwell8/releases/download/dragonwell-8.4.4_jdk8u262-ga/$binary",
-            platform,
-            Some(Alibaba)
-          )
-      }
-      .toList
-      .validate()
-      .insert()
 
   @ChangeSet(
     order = "002",
@@ -50,5 +28,30 @@ class AlibabaDragonwellMigrations {
       .toList
       .validate()
       .insert()
+
+  @ChangeSet(
+    order = "003",
+    id = "003-add_dragonwell-8u272",
+    author = "eddumelendez"
+  )
+  def migrate003(implicit db: MongoDatabase): Unit =
+    Map(
+      Linux64 -> "Alibaba_Dragonwell_8.5.4-GA_Linux_x64.tar.gz"
+    ).map {
+        case (platform, binary) =>
+          Version(
+            "java",
+            "8u272-albba",
+            s"https://github.com/alibaba/dragonwell8/releases/download/dragonwell-8.5.4_jdk8u272-ga/$binary",
+            platform,
+            Some(Alibaba)
+          )
+      }
+      .toList
+      .validate()
+      .insert()
+      .foreach { version =>
+        removeVersion("java", "8u262-albba", version.platform)
+      }
 
 }
