5269e0fdb478718e334c83078e9ec3e8b6bba8ff
Andres Almiray <aalmiray@gmail.com>
2021-01-22 16:34:59 +0100

    Add more detail to Layrry's description

--- src/main/scala/io/sdkman/changelogs/LayrryMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/LayrryMigrations.scala
@@ -16,7 +16,7 @@ class LayrryMigrations {
       candidate = "layrry",
       name = "Layrry",
       description =
-        "Layrry - A Launcher and API for Modularized Java Applications.",
+        "Layrry - A Launcher and API for Modularized Java Applications. It allows to assemble modularized applications based on Maven artifact coordinates of the (modular) JARs to include. Layrry utilizes the Java Module System's notion of module layers, allowing multiple versions of one module to be used within an application at the same time, as well as dynamically adding and removing modules at application runtime.",
       websiteUrl = "https://github.com/moditect/layrry/"
     ).insert()
 }
