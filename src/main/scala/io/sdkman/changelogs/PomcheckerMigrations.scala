95b5805587aca33abfc909b7463a4b3f0d783dad
Andres Almiray <aalmiray@gmail.com>
2021-02-20 21:59:32 +0100

    Add pomchecker candidate

---
+++ src/main/scala/io/sdkman/changelogs/PomcheckerMigrations.scala
@@ -0,0 +1,22 @@
+package io.sdkman.changelogs
+
+import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
+import com.mongodb.client.MongoDatabase
+
+@ChangeLog(order = "043")
+class PomcheckerMigrations {
+
+  @ChangeSet(
+    order = "001",
+    id = "001_add_layrry_candidate",
+    author = "aalmiray"
+  )
+  def migration001(implicit db: MongoDatabase) =
+    Candidate(
+      candidate = "pomchecker",
+      name = "Pomchecker",
+      description =
+        "Pomchecker - Checks that POM files comply with the minimum rules required for publication to Maven Central. It can also check if a POM is a valid BOM file.",
+      websiteUrl = "https://github.com/kordamp/pomchecker/"
+    ).insert()
+}
