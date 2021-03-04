a3e7db5db5383b4ef3fdb61876ce98a898f5d22c
Mykola <golubev.nikolay@gmail.com>
2021-03-03 21:29:37 -0500

    code review feedback

--- src/main/scala/io/sdkman/changelogs/WebtauMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/WebtauMigrations.scala
@@ -18,18 +18,4 @@ class WebtauMigrations {
         "webtau - web test automation. test across REST-API/Graph QL/Browser/Database/CLI/Business-Logic",
       websiteUrl = "https://github.com/testingisdocumenting/webtau"
     ).insert()
-
-  @ChangeSet(
-    order = "002",
-    id = "002_register_version",
-    author = "MykolaGolubyev"
-  )
-  def migration002(implicit db: MongoDatabase) =
-    Version(
-      "webtau",
-      "1.38.0",
-      "https://repo1.maven.org/maven2/org/testingisdocumenting/webtau/webtau-dist/1.38/webtau-dist-1.38-webtau.zip"
-    ).validate()
-      .insert()
-      .asCandidateDefault()
 }
