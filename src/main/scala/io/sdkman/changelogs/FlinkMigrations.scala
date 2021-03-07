c8dbe2e0757ee5b5b3173a4766488e0678e19c67
Marco Vermeulen <vermeulen.mp@gmail.com>
2021-03-07 13:01:07 +0000

    Simplify flink migration.

--- src/main/scala/io/sdkman/changelogs/FlinkMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/FlinkMigrations.scala
@@ -28,84 +28,51 @@ class FlinkMigrations {
     author = "ChethanUK"
   )
   def migration002(implicit db: MongoDatabase) = {
-    flinkListVersion(majorVersion = "1.7", minorVersions = (0 to 2).toList)
-    flinkListVersion(majorVersion = "1.8", minorVersions = (0 to 3).toList)
+    val flinkVersions = List(
+      "1.7.0",
+      "1.7.1",
+      "1.7.2",
+      "1.8.0",
+      "1.8.1",
+      "1.8.2",
+      "1.8.3",
+      "1.9.0",
+      "1.9.1",
+      "1.9.2",
+      "1.9.3",
+      "1.10.0",
+      "1.10.1",
+      "1.10.2",
+      "1.10.3",
+      "1.11.0",
+      "1.11.1",
+      "1.11.2",
+      "1.11.3",
+      "1.12.0",
+      "1.12.1",
+      "1.12.2"
+    )
+    List("2.11", "2.12")
+      .flatMap { scalaVersion =>
+        flinkVersions.map { version =>
+          flinkVersion(version, scalaVersion)
+        }
+      }
+      .validate()
       .insert()
   }
-
-  @ChangeSet(
-    order = "003",
-    id = "003-add_flink_1.9_2.12",
-    author = "ChethanUK"
-  )
-  def migration003(implicit db: MongoDatabase) = {
-    flinkListVersion(majorVersion = "1.9", minorVersions = (0 to 3).toList)
-      .insert()
-  }
-
-  @ChangeSet(
-    order = "004",
-    id = "004-add_flink_1.10_2.12",
-    author = "ChethanUK"
-  )
-  def migration004(implicit db: MongoDatabase) = {
-    flinkListVersion(majorVersion = "1.10", minorVersions = (0 to 3).toList)
-      .insert()
-  }
-
-  @ChangeSet(
-    order = "005",
-    id = "005-add_flink_1.11_2.12",
-    author = "ChethanUK"
-  )
-  def migration005(implicit db: MongoDatabase) = {
-    flinkListVersion(majorVersion = "1.11", minorVersions = (0 to 3).toList)
-      .insert()
-  }
-
-  @ChangeSet(
-    order = "006",
-    id = "006-add_flink_1.12_2.12",
-    author = "ChethanUK"
-  )
-  def migration006(implicit db: MongoDatabase) = {
-    flinkListVersion(majorVersion = "1.12", minorVersions = (0 to 1).toList)
-      .insert()
-  }
-
-  @ChangeSet(
-    order = "007",
-    id = "007-add_flink_1.12.2_2.12_as_default",
-    author = "ChethanUK"
-  )
-  def migration007(implicit db: MongoDatabase) = {
-    flinkVersion("1.12.2", "2.12")
-      .insert()
-      .asCandidateDefault()
-  }
-
-  private def url(version: String, scala: String) =
-    s"https://archive.apache.org/dist/flink/flink-$version/flink-$version-bin-scala_$scala.tgz"
 
   private def flinkVersion(
       flinkVersion: String,
-      scalaVersion: String,
-      candidate: String = "flink"
+      scalaVersion: String
   ) =
     Version(
-      candidate = candidate,
+      candidate = "flink",
       version = flinkVersion,
       url = url(flinkVersion, scalaVersion)
     )
 
-  private def flinkListVersion(
-      majorVersion: String,
-      minorVersions: List[Int],
-      scalaVersion: String = "2.12"
-  ) = {
-    ((minorVersions map { s"$majorVersion." + _ }) map { v =>
-      flinkVersion(v, scalaVersion)
-    }).validate()
-  }
+  private def url(version: String, scala: String) =
+    s"https://archive.apache.org/dist/flink/flink-$version/flink-$version-bin-scala_$scala.tgz"
 
 }
