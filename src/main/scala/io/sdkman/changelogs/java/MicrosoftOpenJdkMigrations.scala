b1b23569ce7fe3928451a1fe097c71f35caba44d
Eddú Meléndez <eddu.melendez@gmail.com>
2021-04-22 13:30:52 -0500

    Add Microsoft JDK 11.0.11.9.1

--- src/main/scala/io/sdkman/changelogs/java/MicrosoftOpenJdkMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/java/MicrosoftOpenJdkMigrations.scala
@@ -12,7 +12,7 @@ class MicrosoftOpenJdkMigrations {
     id = "001-add-microsoft-11.0.10.9",
     author = "eddumelendez"
   )
-  def migrate084(implicit db: MongoDatabase): Unit =
+  def migrate001(implicit db: MongoDatabase): Unit =
     Map(
       Linux64 -> "microsoft-jdk-11.0.10.9-linux-x64.tar.gz",
       MacOSX  -> "microsoft-jdk-11.0.10.9-macos-x64.tar.gz",
@@ -22,6 +22,30 @@ class MicrosoftOpenJdkMigrations {
           Version(
             "java",
             "11.0.10.9-ms",
+            s"https://aka.ms/download-jdk/$binary",
+            platform,
+            Some(Microsoft)
+          )
+      }
+      .toList
+      .validate()
+      .insert()
+
+  @ChangeSet(
+    order = "002",
+    id = "002-add-microsoft-11.0.11.9.1",
+    author = "eddumelendez"
+  )
+  def migrate002(implicit db: MongoDatabase): Unit =
+    Map(
+      Linux64 -> "microsoft-jdk-11.0.11.9.1-linux-x64.tar.gz",
+      MacOSX  -> "microsoft-jdk-11.0.11.9.1-macos-x64.tar.gz",
+      Windows -> "microsoft-jdk-11.0.11.9.1-windows-x64.zip"
+    ).map {
+        case (platform, binary) =>
+          Version(
+            "java",
+            "11.0.11.9.1-ms",
             s"https://aka.ms/download-jdk/$binary",
             platform,
             Some(Microsoft)
