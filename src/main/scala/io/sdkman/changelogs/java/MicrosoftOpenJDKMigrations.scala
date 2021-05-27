1c3c66a48b72ccccd19eab0444739a2b70e078bb
Eddú Meléndez <eddu.melendez@gmail.com>
2021-05-27 12:43:42 -0500

    Add Microsoft JDK 16.0.1.9.1

---
+++ src/main/scala/io/sdkman/changelogs/java/MicrosoftOpenJDKMigrations.scala
@@ -0,0 +1,34 @@
+package io.sdkman.changelogs.java
+
+import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
+import com.mongodb.client.MongoDatabase
+import io.sdkman.changelogs.{Linux64, MacOSX, Microsoft, Version, Windows}
+
+@ChangeLog(order = "047")
+class MicrosoftOpenJdkMigrations {
+
+  @ChangeSet(
+    order = "003",
+    id = "002-add-microsoft-16.0.1.9.1",
+    author = "eddumelendez"
+  )
+  def migrate002(implicit db: MongoDatabase): Unit =
+    Map(
+      Linux64 -> "microsoft-jdk-16.0.1.9.1-linux-x64.tar.gz",
+      MacOSX  -> "microsoft-jdk-16.0.1.9.1-macOS-x64.tar.gz",
+      Windows -> "microsoft-jdk-16.0.1.9.1-windows-x64.zip"
+    ).map {
+        case (platform, binary) =>
+          Version(
+            "java",
+            "16.0.1.9.1-ms",
+            s"https://aka.ms/download-jdk/$binary",
+            platform,
+            Some(Microsoft)
+          )
+      }
+      .toList
+      .validate()
+      .insert()
+
+}
