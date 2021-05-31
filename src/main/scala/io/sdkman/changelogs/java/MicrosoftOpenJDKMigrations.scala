7a8f59c4a01590ad4790c6923da77afad3a714ed
Eddú Meléndez <eddu.melendez@gmail.com>
2021-05-31 10:55:09 -0500

    fix

--- src/main/scala/io/sdkman/changelogs/java/MicrosoftOpenJDKMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/java/MicrosoftOpenJDKMigrations.scala
@@ -12,7 +12,7 @@ class MicrosoftOpenJdkMigrations {
     id = "002-add-microsoft-16.0.1.9.1",
     author = "eddumelendez"
   )
-  def migrate002(implicit db: MongoDatabase): Unit =
+  def migrate003(implicit db: MongoDatabase): Unit =
     Map(
       Linux64 -> "microsoft-jdk-16.0.1.9.1-linux-x64.tar.gz",
       MacOSX  -> "microsoft-jdk-16.0.1.9.1-macOS-x64.tar.gz",
