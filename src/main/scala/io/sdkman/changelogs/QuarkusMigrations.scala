9191c0a5d86bfc3ba063be9470dbda3ace11cd16
Max Rydahl Andersen <manderse@redhat.com>
2021-07-10 13:57:25 +0200

    Adding entry for Quarkus (#522)

---
+++ src/main/scala/io/sdkman/changelogs/QuarkusMigrations.scala
@@ -0,0 +1,22 @@
+package io.sdkman.changelogs
+
+import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
+import com.mongodb.client.MongoDatabase
+
+@ChangeLog(order = "053")
+class QuarkusMigrations {
+
+  @ChangeSet(
+    order = "001",
+    id = "001_add_quarkus_candidate",
+    author = "maxandersen"
+  )
+  def migration001(implicit db: MongoDatabase) =
+    Candidate(
+      candidate = "quarkus",
+      name = "Quarkus CLI",
+      description =
+        "Quarkus is a Kubernetes Native Java framework tailored for OpenJDK HotSpot and GraalVM, crafted from best-of-breed Java libraries and standards.",
+      websiteUrl = "http://quarkus.io"
+    ).insert()
+}
