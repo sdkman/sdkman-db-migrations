90c1e6f045821ee7a1239b3e503c064edac53e83
Foivos Zakkak <fzakkak@redhat.com>
2021-07-20 13:20:04 +0300

    Add Mandrel 21.2.0 and 20.3.3, hide 21.1.0 and 20.3.2

--- src/main/scala/io/sdkman/changelogs/java/MandrelMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/java/MandrelMigrations.scala
@@ -173,4 +173,35 @@ class MandrelMigrations {
       .insert()
   }
 
+  @ChangeSet(
+    order = "007",
+    id = "007-hide_mandrel-20_3_2_0-21_1_0_0-add_mandrel-20_3_3_0-21_2_0_0",
+    author = "zakkak"
+  )
+  def migrate007(implicit db: MongoDatabase): Unit = {
+    hideVersion("java", "20.3.2.0-mandrel")
+    hideVersion("java", "21.1.0.0-mandrel")
+    var platforms = List(
+      (Linux64, "linux-amd64", "tar.gz"),
+      (Windows, "windows-amd64", "zip")
+    )
+    var versions = List("20.3.3.0", "21.2.0.0")
+    versions
+      .map { version =>
+        platforms.map {
+          case (platform, mandrelPlatform, suffix) =>
+            Version(
+              candidate = "java",
+              version = s"$version-mandrel",
+              url =
+                s"https://github.com/graalvm/mandrel/releases/download/mandrel-$version-Final/mandrel-java11-$mandrelPlatform-$version-Final.$suffix",
+              platform = platform,
+              vendor = Some(Mandrel)
+            )
+        }
+      }
+      .validate()
+      .insert()
+  }
+
 }
