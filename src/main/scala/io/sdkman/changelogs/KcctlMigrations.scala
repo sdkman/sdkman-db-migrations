ff89ae933a11c95d9e26599da3e76fab608bb622
Gunnar Morling <gunnar.morling@googlemail.com>
2021-12-05 13:44:28 +0100

    Adding kcctl candidate

---
+++ src/main/scala/io/sdkman/changelogs/KcctlMigrations.scala
@@ -0,0 +1,22 @@
+package io.sdkman.changelogs
+
+import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
+import com.mongodb.client.MongoDatabase
+
+@ChangeLog(order = "064")
+class KcctlMigrations {
+
+  @ChangeSet(
+    order = "001",
+    id = "001_add_kcctl_candidate",
+    author = "gunnarmorling"
+  )
+  def migration001(implicit db: MongoDatabase) =
+    Candidate(
+      candidate = "kcctl",
+      name = "kcctl",
+      description =
+        "kcctl - a modern and intuitive command-line client for Kafka Connect. Relying on the idioms and semantics of kubectl, it allows you to register and examine connectors, delete them, restart them, etc.",
+      websiteUrl = "https://github.com/kcctl/kcctl"
+    ).insert()
+}
