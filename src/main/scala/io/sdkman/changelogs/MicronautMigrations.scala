2beb86903f0d3a97ad3bdc138488c38ddee365c9
Marco Vermeulen <vermeulen.mp@gmail.com>
2021-12-05 18:41:30 +0000

    Removes LOTS of obsolete migrations plus other cleanup.

--- src/main/scala/io/sdkman/changelogs/MicronautMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/MicronautMigrations.scala
@@ -11,7 +11,7 @@ class MicronautMigrations {
     id = "001_add_micronaut_candidate",
     author = "jameskleeh"
   )
-  def migration001(implicit db: MongoDatabase) = {
+  def migration001(implicit db: MongoDatabase): Candidate = {
     Candidate(
       candidate = "micronaut",
       name = "Micronaut",
@@ -19,59 +19,5 @@ class MicronautMigrations {
         "Micronaut is an open source microservice framework for the JVM",
       websiteUrl = "http://micronaut.io/"
     ).insert()
-  }
-
-  @ChangeSet(
-    order = "002",
-    id = "002_replace_2_0_0_M3_with_platform_binaries",
-    author = "marc0der"
-  )
-  def migration002(implicit db: MongoDatabase) = {
-    removeVersion("micronaut", "2.0.0.M3", Universal)
-    List(
-      Version(
-        "micronaut",
-        "2.0.0.M3",
-        "https://github.com/micronaut-projects/micronaut-starter/releases/download/v2.0.0.M3/mn-darwin-amd64-v2.0.0.M3.tar.gz",
-        MacOSX
-      ),
-      Version(
-        "micronaut",
-        "2.0.0.M3",
-        "https://github.com/micronaut-projects/micronaut-starter/releases/download/v2.0.0.M3/mn-linux-amd64-v2.0.0.M3.tar.gz",
-        Linux64
-      ),
-      Version(
-        "micronaut",
-        "2.0.0.M3",
-        "https://github.com/micronaut-projects/micronaut-starter/releases/download/v2.0.0.M3/mn-win-amd64-v2.0.0.M3.zip",
-        Windows
-      )
-    ).validate()
-      .insert()
-  }
-
-  @ChangeSet(
-    order = "003",
-    id = "002_replace_2_0_0_M3_with_platform_zip_binaries",
-    author = "marc0der"
-  )
-  def migration003(implicit db: MongoDatabase) = {
-    Seq(Linux64, MacOSX).foreach(removeVersion("micronaut", "2.0.0.M3", _))
-    List(
-      Version(
-        "micronaut",
-        "2.0.0.M3",
-        "https://github.com/micronaut-projects/micronaut-starter/releases/download/v2.0.0.M3/mn-darwin-amd64-v2.0.0.M3.zip",
-        MacOSX
-      ),
-      Version(
-        "micronaut",
-        "2.0.0.M3",
-        "https://github.com/micronaut-projects/micronaut-starter/releases/download/v2.0.0.M3/mn-linux-amd64-v2.0.0.M3.zip",
-        Linux64
-      )
-    ).validate()
-      .insert()
   }
 }
--- src/main/scala/io/sdkman/changelogs/VisualVmMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/VisualVmMigrations.scala
@@ -5,153 +5,12 @@ import com.mongodb.client.MongoDatabase
 
 @ChangeLog(order = "009")
 class VisualVmMigrations {
-
-  @ChangeSet(order = "001", id = "001-add_visualvm_1_4_1", author = "vpavic")
-  def migration001(implicit db: MongoDatabase) = {
-    Version(
-      "visualvm",
-      "1.4.1",
-      "https://github.com/visualvm/visualvm.src/releases/download/1.4.1/visualvm_141.zip"
-    ).insert()
-      .asCandidateDefault()
-  }
-
-  @ChangeSet(order = "002", id = "002-add_visualvm_1_4_2", author = "vpavic")
-  def migration002(implicit db: MongoDatabase) = {
-    Version(
-      "visualvm",
-      "1.4.2",
-      "https://github.com/visualvm/visualvm.src/releases/download/1.4.2/visualvm_142.zip"
-    ).validate()
-      .insert()
-      .asCandidateDefault()
-  }
-
-  @ChangeSet(order = "003", id = "003-add_visualvm_1_4_3", author = "jorsol")
-  def migration003(implicit db: MongoDatabase) = {
-    Version(
-      "visualvm",
-      "1.4.3",
-      "https://github.com/visualvm/visualvm.src/releases/download/1.4.3/visualvm_143.zip"
-    ).validate()
-      .insert()
-      .asCandidateDefault()
-  }
-
-  @ChangeSet(order = "004", id = "004-add_visualvm_1_4_4", author = "Markitox")
-  def migration004(implicit db: MongoDatabase) = {
-    Version(
-      "visualvm",
-      "1.4.4",
-      "https://github.com/oracle/visualvm/releases/download/1.4.4/visualvm_144.zip"
-    ).validate()
-      .insert()
-      .asCandidateDefault()
-  }
-
-  @ChangeSet(order = "005", id = "005-add_visualvm_2_0", author = "ramonPires")
-  def migration005(implicit db: MongoDatabase) = {
-    Version(
-      "visualvm",
-      "2.0",
-      "https://github.com/oracle/visualvm/releases/download/2.0/visualvm_20.zip"
-    ).validate()
-      .insert()
-      .asCandidateDefault()
-  }
-
-  @ChangeSet(
-    order = "006",
-    id = "006-add_visualvm_2_0_2",
-    author = "eddumelendez"
-  )
-  def migration006(implicit db: MongoDatabase) = {
-    Version(
-      "visualvm",
-      "2.0.2",
-      "https://github.com/oracle/visualvm/releases/download/2.0.2/visualvm_202.zip"
-    ).validate()
-      .insert()
-      .asCandidateDefault()
-  }
-
-  @ChangeSet(order = "007", id = "007-add_visualvm_2_0_4", author = "izeye")
-  def migration007(implicit db: MongoDatabase) = {
-    Version(
-      "visualvm",
-      "2.0.4",
-      "https://github.com/oracle/visualvm/releases/download/2.0.4/visualvm_204.zip"
-    ).validate()
-      .insert()
-      .asCandidateDefault()
-  }
-
-  @ChangeSet(
-    order = "008",
-    id = "008-add_visualvm_2_0_5",
-    author = "eddumelendez"
-  )
-  def migration008(implicit db: MongoDatabase) = {
-    Version(
-      "visualvm",
-      "2.0.5",
-      "https://github.com/oracle/visualvm/releases/download/2.0.5/visualvm_205.zip"
-    ).validate()
-      .insert()
-      .asCandidateDefault()
-  }
-
-  @ChangeSet(
-    order = "009",
-    id = "009-add_visualvm_2_0_6",
-    author = "eddumelendez"
-  )
-  def migration009(implicit db: MongoDatabase) = {
-    Version(
-      "visualvm",
-      "2.0.6",
-      "https://github.com/oracle/visualvm/releases/download/2.0.6/visualvm_206.zip"
-    ).validate()
-      .insert()
-      .asCandidateDefault()
-  }
-
-  @ChangeSet(
-    order = "010",
-    id = "010-add_visualvm_2_0_7",
-    author = "eddumelendez"
-  )
-  def migration010(implicit db: MongoDatabase) = {
-    Version(
-      "visualvm",
-      "2.0.7",
-      "https://github.com/oracle/visualvm/releases/download/2.0.7/visualvm_207.zip"
-    ).validate()
-      .insert()
-      .asCandidateDefault()
-  }
-
-  @ChangeSet(
-    order = "011",
-    id = "011-add_visualvm_2_1",
-    author = "eddumelendez"
-  )
-  def migration011(implicit db: MongoDatabase) = {
-    Version(
-      "visualvm",
-      "2.1",
-      "https://github.com/oracle/visualvm/releases/download/2.1/visualvm_21.zip"
-    ).validate()
-      .insert()
-      .asCandidateDefault()
-  }
-
   @ChangeSet(
     order = "012",
     id = "012-add_visualvm_2_1_1",
     author = "eddumelendez"
   )
-  def migration012(implicit db: MongoDatabase) = {
+  def migration012(implicit db: MongoDatabase): Unit = {
     Version(
       "visualvm",
       "2.1.1",
@@ -160,5 +19,4 @@ class VisualVmMigrations {
       .insert()
       .asCandidateDefault()
   }
-
 }
