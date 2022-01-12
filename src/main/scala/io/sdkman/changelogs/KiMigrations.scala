72a0d40e9acf1c93a65853c791dd0bef43ee47fb
Marco Vermeulen <vermeulen.mp@gmail.com>
2022-01-12 13:59:25 +0000

    Fixes an improvements on ki migration.

--- src/main/scala/io/sdkman/changelogs/KiMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/KiMigrations.scala
@@ -3,7 +3,7 @@ package io.sdkman.changelogs
 import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
 import com.mongodb.client.MongoDatabase
 
-@ChangeLog(order = "065")
+@ChangeLog(order = "066")
 class KiMigrations {
   @ChangeSet(order = "001", id = "001-add_ki_candidate", author = "asm0dey")
   def migrate001(implicit db: MongoDatabase) = {
@@ -11,7 +11,7 @@ class KiMigrations {
       candidate = "ki",
       name = "ki",
       description =
-        "The shell is an extensible implementation of Kotlin REPL with reach set of features",
+        "An extensible implementation of the Kotlin REPL with a rich set of features including autocomplete, syntax highlighting, type inference and maven dependencies.",
       websiteUrl = "https://github.com/Kotlin/kotlin-interactive-shell"
     ).insert()
   }
