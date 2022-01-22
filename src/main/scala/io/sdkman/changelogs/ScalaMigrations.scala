9288b37fe049701dd941851506b06d01fccf939c
helpermethod <oliver@sdkman.io>
2022-01-22 06:46:41 +0100

    Remove old Scala migrations

--- src/main/scala/io/sdkman/changelogs/ScalaMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/ScalaMigrations.scala
@@ -6,32 +6,6 @@ import com.mongodb.client.MongoDatabase
 @ChangeLog(order = "003")
 class ScalaMigrations {
   @ChangeSet(
-    order = "027",
-    id = "027_add_scala_2.13.7",
-    author = "helpermethod"
-  )
-  def migration027(implicit db: MongoDatabase): Unit =
-    Version(
-      candidate = "scala",
-      version = "2.13.7",
-      url = "https://downloads.lightbend.com/scala/2.13.7/scala-2.13.7.zip"
-    ).validate()
-      .insert()
-
-  @ChangeSet(
-    order = "028",
-    id = "028_add_scala_2.13.8",
-    author = "helpermethod"
-  )
-  def migration028(implicit db: MongoDatabase): Unit =
-    Version(
-      candidate = "scala",
-      version = "2.13.8",
-      url = "https://downloads.lightbend.com/scala/2.13.8/scala-2.13.8.zip"
-    ).validate()
-      .insert()
-
-  @ChangeSet(
     order = "029",
     id = "029_add_scala_3.1.1",
     author = "helpermethod"
