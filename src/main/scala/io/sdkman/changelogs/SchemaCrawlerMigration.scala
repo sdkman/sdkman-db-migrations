c087c8d9e84d09645a3ce60e06b0dee488303ed9
Marco Vermeulen <vermeulen.mp@gmail.com>
2022-01-25 19:06:55 +0000

    Fix code formatting.

--- src/main/scala/io/sdkman/changelogs/SchemaCrawlerMigration.scala
+++ src/main/scala/io/sdkman/changelogs/SchemaCrawlerMigration.scala
@@ -16,12 +16,12 @@ class SchemaCrawlerMigrations {
       name = "SchemaCrawler",
       description =
         "SchemaCrawler is a free database schema discovery and comprehension tool. SchemaCrawler " +
-        "has a good mix of useful features for data governance. You can search for database " +
-        "schema objects using regular expressions, and output the schema and data in a readable " +
-        "text format. The output serves for database documentation, and is designed to be " +
-        "diff-ed against other database schemas. SchemaCrawler also generates schema diagrams. " +
-        "You can execute scripts in any standard scripting language against your database. You " +
-        "can find potential schema design issues with lint.",
+          "has a good mix of useful features for data governance. You can search for database " +
+          "schema objects using regular expressions, and output the schema and data in a readable " +
+          "text format. The output serves for database documentation, and is designed to be " +
+          "diff-ed against other database schemas. SchemaCrawler also generates schema diagrams. " +
+          "You can execute scripts in any standard scripting language against your database. You " +
+          "can find potential schema design issues with lint.",
       websiteUrl = "https://www.schemacrawler.com"
     ).insert()
   }
