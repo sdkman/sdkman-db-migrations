daba6d9ec7faf3e7f7d24563b8e8dc45351dd124
shyam <xshyamx@gmail.com>
2022-01-28 11:55:34 +0530

    updating new jmeter versions and setting new default

--- src/main/scala/io/sdkman/changelogs/JMeterMigration.scala
+++ src/main/scala/io/sdkman/changelogs/JMeterMigration.scala
@@ -2,21 +2,41 @@ package io.sdkman.changelogs
 
 import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
 import com.mongodb.client.MongoDatabase
+import org.bson.Document
 
 @ChangeLog(order = "051")
 class JMeterMigration {
+  val CandidateName  = "jmeter"
+  val DefaultVersion = "5.4.3"
+
   @ChangeSet(
-    order = "001",
-    id = "001-add_jmeter_candidate",
+    order = "003",
+    id = "003-add_latest_jmeter_candidate_urls",
     author = "xshyamx"
   )
-  def migration001(implicit db: MongoDatabase): Candidate = {
-    Candidate(
-      candidate = "jmeter",
-      name = "Apache JMeter",
-      description =
-        " The Apache JMeter™ application is open source software, a 100% pure Java application designed to load test functional behavior and measure performance. It was originally designed for testing Web Applications but has since expanded to other test functions.",
-      websiteUrl = "https://jmeter.apache.org/"
-    ).insert()
+  def migration003(implicit db: MongoDatabase): List[Version] = {
+    List(
+      DefaultVersion,
+      "5.4.2"
+    ).map(
+        version =>
+          Version(
+            candidate = CandidateName,
+            version = version,
+            url =
+              s"https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-$version.zip"
+          )
+      )
+      .validate()
+      .insert()
   }
+  @ChangeSet(
+    order = "004",
+    id = "004-update-default-jmeter-version",
+    author = "xshyamx"
+  )
+  def migration004(implicit db: MongoDatabase): Document = {
+    setCandidateDefault(CandidateName, DefaultVersion)
+  }
+
 }
