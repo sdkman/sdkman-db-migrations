f1009b5b482de1fc3065008bf01c2b02635d5b11
shyam <xshyamx@gmail.com>
2022-01-29 09:45:37 +0530

    including candidate changeset

--- src/main/scala/io/sdkman/changelogs/JMeterMigration.scala
+++ src/main/scala/io/sdkman/changelogs/JMeterMigration.scala
@@ -8,6 +8,21 @@ import org.bson.Document
 class JMeterMigration {
 
   @ChangeSet(
+    order = "001",
+    id = "001-add_jmeter_candidate",
+    author = "xshyamx"
+  )
+  def migration001(implicit db: MongoDatabase): Candidate = {
+    Candidate(
+      candidate = "jmeter",
+      name = "Apache JMeter",
+      description =
+        "The Apache JMeter™ application is open source software, a 100% pure Java application designed to load test functional behavior and measure performance. It was originally designed for testing Web Applications but has since expanded to other test functions.",
+      websiteUrl = "https://jmeter.apache.org/"
+    ).insert()
+  }
+
+  @ChangeSet(
     order = "003",
     id = "003-add_latest_jmeter_candidate_urls",
     author = "xshyamx"
