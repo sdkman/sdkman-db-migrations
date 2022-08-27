d6d2b045814faca0726a7a1ce5cda711c4d4b0a3
Marco Vermeulen <vermeulen.mp@gmail.com>
2022-08-27 12:27:38 +0100

    Fix broken build.

--- src/main/scala/io/sdkman/changelogs/ToolkitMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/ToolkitMigrations.scala
@@ -18,34 +18,8 @@ class ToolkitMigrations {
       candidate = CandidateName,
       name = "Toolkit Migrations",
       description =
-        "Toolkit is a client command line tool that supports multiple IoT protocols. for example, MQTT and COAP protocols" +
-          "Provide a user optimized client tool for IoT developers and learners",
+        "Toolkit is a client command line tool that supports multiple IoT protocols like MQTT and COAP. It provides a command line client tool for IoT development.",
       websiteUrl = "https://github.com/IoT-Technology/IoT-Toolkit"
     ).insert()
   }
-
-  @ChangeSet(
-    order = "002",
-    id = "002-add_toolkit_0.4.0_as_default",
-    author = "sanshengshui"
-  )
-  def migration002(implicit db: MongoDatabase) =
-    Map(
-      MacOSX  -> "toolkit-0.4.0-osx-x86_64.zip",
-      Linux64 -> "toolkit-0.4.0-linux-x86_64.zip",
-      Windows -> "toolkit-0.4.0-windows-x86_64.zip"
-    ).map {
-        case (platform, binary) =>
-          Version(
-            "toolkit",
-            "0.4.0",
-            s"https://github.com/IoT-Technology/IoT-Toolkit/releases/download/0.4.0/$binary",
-            platform
-          )
-      }
-      .toList
-      .validate()
-      .insert()
-      .asCandidateDefault()
-
 }
