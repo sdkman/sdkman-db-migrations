f003f2837c0f24b1b1cad2d521403a641aa3b619
Chris Hebert <8251796+cphbrt@users.noreply.github.com>
2022-11-16 14:17:12 -0600

    Add Spark 3.2.2 to 3.3.1

--- src/main/scala/io/sdkman/changelogs/SparkMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/SparkMigrations.scala
@@ -6,31 +6,45 @@ import com.mongodb.client.MongoDatabase
 @ChangeLog(order = "007")
 class SparkMigrations {
   @ChangeSet(
-    order = "017",
-    id = "016-add_spark_3.2.0",
-    author = "ChethanUK"
+    order = "020",
+    id = "020-add_spark_3.2.2",
+    author = "cphbrt"
   )
-  def migration017(implicit db: MongoDatabase): Unit = {
+  def migration020(implicit db: MongoDatabase) = {
     Version(
       "spark",
-      "3.2.0",
-      "https://archive.apache.org/dist/spark/spark-3.2.0/spark-3.2.0-bin-hadoop3.2.tgz"
+      "3.2.2",
+      "https://archive.apache.org/dist/spark/spark-3.2.2/spark-3.2.2-bin-hadoop3.2.tgz"
     ).validate()
       .insert()
-      .asCandidateDefault()
   }
 
   @ChangeSet(
-    order = "019",
-    id = "019-add_spark_3.2.1",
-    author = "ChethanUK"
+    order = "021",
+    id = "021-add_spark_3.3.0",
+    author = "cphbrt"
   )
-  def migration019(implicit db: MongoDatabase) = {
+  def migration021(implicit db: MongoDatabase) = {
     Version(
       "spark",
-      "3.2.1",
-      "https://archive.apache.org/dist/spark/spark-3.2.1/spark-3.2.1-bin-hadoop3.2.tgz"
+      "3.3.0",
+      "https://archive.apache.org/dist/spark/spark-3.3.0/spark-3.3.0-bin-hadoop3.tgz"
     ).validate()
       .insert()
+  }
+
+  @ChangeSet(
+    order = "022",
+    id = "022-add_spark_3.3.1",
+    author = "cphbrt"
+  )
+  def migration022(implicit db: MongoDatabase) = {
+    Version(
+      "spark",
+      "3.3.1",
+      "https://archive.apache.org/dist/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz"
+    ).validate()
+      .insert()
+      .asCandidateDefault()
   }
 }
