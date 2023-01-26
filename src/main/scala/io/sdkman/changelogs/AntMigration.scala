502929cee898f1c80867b31bf84c69f9517186bc
Andres Almiray <aalmiray@gmail.com>
2023-01-26 20:16:02 +0100

    Add Ant 1.10.13

--- src/main/scala/io/sdkman/changelogs/AntMigration.scala
+++ src/main/scala/io/sdkman/changelogs/AntMigration.scala
@@ -6,30 +6,15 @@ import com.mongodb.client.MongoDatabase
 @ChangeLog(order = "032")
 class AntMigration {
   @ChangeSet(
-    order = "016",
-    id = "016-add_ant_1.10.11",
-    author = "aalmiray"
-  )
-  def migration016(implicit db: MongoDatabase): Unit = {
-    Version(
-      "ant",
-      "1.10.11",
-      "https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.11-bin.zip"
-    ).validate()
-      .insert()
-      .asCandidateDefault()
-  }
-
-  @ChangeSet(
-    order = "017",
-    id = "017-add_ant_1.10.12",
+    order = "018",
+    id = "018-add_ant_1.10.13",
     author = "aalmiray"
   )
   def migration017(implicit db: MongoDatabase): Unit = {
     Version(
       "ant",
-      "1.10.12",
-      "https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.12-bin.zip"
+      "1.10.13",
+      "https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.13-bin.zip"
     ).validate()
       .insert()
       .asCandidateDefault()
