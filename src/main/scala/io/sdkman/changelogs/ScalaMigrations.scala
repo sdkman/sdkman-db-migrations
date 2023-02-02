937ed153b6614cb603f67fec0013007cd4342774
Łukasz Wroński <lukaszwronski98@gmail.com>
2023-02-02 13:30:26 +0100

    Add Scala 3.2.2

--- src/main/scala/io/sdkman/changelogs/ScalaMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/ScalaMigrations.scala
@@ -6,16 +6,16 @@ import com.mongodb.client.MongoDatabase
 @ChangeLog(order = "003")
 class ScalaMigrations {
   @ChangeSet(
-    order = "029",
-    id = "029_add_scala_3.1.1",
-    author = "helpermethod"
+    order = "030",
+    id = "030_add_scala_3.2.2",
+    author = "lwronski"
   )
-  def migration029(implicit db: MongoDatabase): Unit =
+  def migration030(implicit db: MongoDatabase): Unit =
     Version(
       candidate = "scala",
-      version = "3.1.1",
+      version = "3.2.2",
       url =
-        "https://github.com/lampepfl/dotty/releases/download/3.1.1/scala3-3.1.1.zip"
+        "https://github.com/lampepfl/dotty/releases/download/3.2.2/scala3-3.2.2.zip"
     ).validate()
       .insert()
       .asCandidateDefault()
