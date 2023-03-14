215cfabf44b6aa53a45edc072b1350ec1e02f30d
Andres Almiray <aalmiray@gmail.com>
2023-03-14 20:41:15 +0100

    Add Maven 3.8.8

--- src/main/scala/io/sdkman/changelogs/MavenMigration.scala
+++ src/main/scala/io/sdkman/changelogs/MavenMigration.scala
@@ -6,15 +6,15 @@ import com.mongodb.client.MongoDatabase
 @ChangeLog(order = "013")
 class MavenMigration {
   @ChangeSet(
-    order = "023",
-    id = "023-add_maven_4.0.0_alpha_4",
+    order = "024",
+    id = "024-add_maven_3.8.8",
     author = "aalmiray"
   )
-  def migration023(implicit db: MongoDatabase): Unit = {
+  def migration024(implicit db: MongoDatabase): Unit = {
     Version(
       "maven",
-      "4.0.0-alpha-4",
-      "https://dlcdn.apache.org/maven/maven-4/4.0.0-alpha-4/binaries/apache-maven-4.0.0-alpha-4-bin.zip"
+      "3.8.8",
+      "https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.zip"
     ).validate()
       .insert()
   }
