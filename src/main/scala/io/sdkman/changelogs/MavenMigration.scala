4a61c5f09edd371f25da8225b3d7f4368cc0f55a
Andres Almiray <aalmiray@gmail.com>
2023-03-21 21:56:48 +0100

    Add Maven 3.9.1 as default (#635)

--- src/main/scala/io/sdkman/changelogs/MavenMigration.scala
+++ src/main/scala/io/sdkman/changelogs/MavenMigration.scala
@@ -6,16 +6,18 @@ import com.mongodb.client.MongoDatabase
 @ChangeLog(order = "013")
 class MavenMigration {
   @ChangeSet(
-    order = "026",
-    id = "026-add_maven_4.0.0-alpha-5",
+    order = "027",
+    id = "027-add_maven_3.9.1",
     author = "aalmiray"
   )
-  def migration026(implicit db: MongoDatabase): Unit = {
+  def migration027(implicit db: MongoDatabase): Unit = {
+    removeVersion("maven", "3.9.1")
     Version(
       "maven",
-      "4.0.0-alpha-5",
-      "https://dlcdn.apache.org/maven/maven-4/4.0.0-alpha-5/binaries/apache-maven-4.0.0-alpha-5-bin.zip"
+      "3.9.1",
+      "https://dlcdn.apache.org/maven/maven-3/3.9.1/binaries/apache-maven-3.9.1-bin.zip"
     ).validate()
       .insert()
+      .asCandidateDefault()
   }
 }
