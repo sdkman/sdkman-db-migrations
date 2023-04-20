93c384f66655360244a0f1dc4def4d610422db88
Marco Vermeulen <vermeulen.mp@gmail.com>
2023-04-20 08:50:11 +0100

    Scalafmt.

--- src/main/scala/io/sdkman/changelogs/BldMigration.scala
+++ src/main/scala/io/sdkman/changelogs/BldMigration.scala
@@ -14,7 +14,8 @@ class BldMigration {
     Candidate(
       candidate = "bld",
       name = "Bld",
-      description = "bld is a build system that allows you to write your build logic in pure Java. bld was created because we're not really interested in build tools. We use them because we have to, but we'd rather just get on with coding the real stuff.",
+      description =
+        "bld is a build system that allows you to write your build logic in pure Java. bld was created because we're not really interested in build tools. We use them because we have to, but we'd rather just get on with coding the real stuff.",
       websiteUrl = "https://rife2.com/bld",
       distribution = "UNIVERSAL"
     ).insert()
