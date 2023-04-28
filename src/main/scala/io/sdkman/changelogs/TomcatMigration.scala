cd3f53ca9b2b9837d33c8894b0b35de1e77d68c7
Stefan Penndorf <stefan@cyphoria.net>
2023-04-28 11:58:32 +0200

    Add current tomcat versions and remove EOL ones (#628)

--- src/main/scala/io/sdkman/changelogs/TomcatMigration.scala
+++ src/main/scala/io/sdkman/changelogs/TomcatMigration.scala
@@ -72,4 +72,36 @@ class TomcatMigration {
     ).validate()
       .insert()
       .asCandidateDefault()
+
+  @ChangeSet(
+    order = "010",
+    id = "010-update_tomcat_versions",
+    author = "stefanpenndorf"
+  )
+  def migration010(implicit db: MongoDatabase): Document = {
+    removeVersion("tomcat", "10.0.0-M10")
+    removeVersion("tomcat", "10.1.0-M8")
+
+    List(
+      "8"  -> "8.5.86",
+      "8"  -> "8.5.87",
+      "9"  -> "9.0.65",
+      "9"  -> "9.0.72",
+      "9"  -> "9.0.73",
+      "10" -> "10.1.6",
+      "10" -> "10.1.7",
+      "11" -> "11.0.0-M3"
+    ).map {
+        case (series: String, version: String) =>
+          Version(
+            candidate = "tomcat",
+            version = version,
+            url =
+              s"https://archive.apache.org/dist/tomcat/tomcat-$series/v$version/bin/apache-tomcat-$version.zip"
+          )
+      }
+      .validate()
+      .insert()
+    setCandidateDefault("tomcat", "10.1.7")
+  }
 }
