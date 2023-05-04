fd77a2b79e8bbde1770b34e7523288ca27aa9c00
Eliezer Graber <me@eygraber.com>
2023-05-04 12:44:25 -0400

    Fixed author and description

--- src/main/scala/io/sdkman/changelogs/KtlintMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/KtlintMigrations.scala
@@ -5,12 +5,12 @@ import com.mongodb.client.MongoDatabase
 
 @ChangeLog(order = "079")
 class KtlintMigrations {
-  @ChangeSet(order = "001", id = "001_add_ktlint_candidate", author = "paul-dingemans")
+  @ChangeSet(order = "001", id = "001_add_ktlint_candidate", author = "eygraber")
   def migration001(implicit db: MongoDatabase) =
     Candidate(
       candidate = "ktlint",
       name = "ktlint",
-      description = "An anti-bikeshedding Kotlin linter with built-in formatter",
+      description = "Ktlint is an anti-bikeshedding Kotlin linter with built-in formatter.",
       websiteUrl = "https://github.com/pinterest/ktlint"
     ).insert()
 }
