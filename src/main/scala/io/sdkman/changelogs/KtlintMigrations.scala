4c976521ee466126ffbcc161dfb5f15e92dfa77d
Marco Vermeulen <vermeulen.mp@gmail.com>
2023-05-07 15:54:22 +0100

    Run formatter.

--- src/main/scala/io/sdkman/changelogs/KtlintMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/KtlintMigrations.scala
@@ -5,12 +5,17 @@ import com.mongodb.client.MongoDatabase
 
 @ChangeLog(order = "079")
 class KtlintMigrations {
-  @ChangeSet(order = "001", id = "001_add_ktlint_candidate", author = "eygraber")
+  @ChangeSet(
+    order = "001",
+    id = "001_add_ktlint_candidate",
+    author = "eygraber"
+  )
   def migration001(implicit db: MongoDatabase) =
     Candidate(
       candidate = "ktlint",
       name = "ktlint",
-      description = "Ktlint is an anti-bikeshedding Kotlin linter with built-in formatter.",
+      description =
+        "Ktlint is an anti-bikeshedding Kotlin linter with built-in formatter.",
       websiteUrl = "https://github.com/pinterest/ktlint"
     ).insert()
 }
