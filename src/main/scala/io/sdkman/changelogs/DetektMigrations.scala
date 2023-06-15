97f91af32b946aadd56f73674d35dfd676515700
Oliver Weiler <oliver@sdkman.io>
2023-06-15 08:21:15 +0200

    chore: Set Detekt candidate changelog number to 80

--- src/main/scala/io/sdkman/changelogs/DetektMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/DetektMigrations.scala
@@ -3,12 +3,12 @@ package io.sdkman.changelogs
 import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
 import com.mongodb.client.MongoDatabase
 
-@ChangeLog(order = "079")
+@ChangeLog(order = "080")
 class DetektMigrations {
 
   @ChangeSet(
     order = "001",
-    id = "001_add_detect_3_2_4",
+    id = "001_add_detect_1_22_0",
     author = "helpermethod"
   )
   def migration001(implicit db: MongoDatabase): Version = {
@@ -27,5 +27,21 @@ class DetektMigrations {
       url =
         "https://github.com/detekt/detekt/releases/download/v1.22.0/detekt-cli-1.22.0.zip"
     ).insert()
+  }
+
+  @ChangeSet(
+    order = "002",
+    id = "001_add_detect_1_23_0",
+    author = "helpermethod"
+  )
+  def migration002(implicit db: MongoDatabase): Unit = {
+    Version(
+      candidate = "detekt",
+      version = "1.23.0",
+      url =
+        "https://github.com/detekt/detekt/releases/download/v1.23.0/detekt-cli-1.22.0.zip"
+    ).validate()
+      .insert()
+      .asCandidateDefault()
   }
 }
