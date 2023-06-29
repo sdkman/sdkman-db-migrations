567bc1ee73442c7b68b8fae40803126561e228c2
Andres Almiray <aalmiray@gmail.com>
2023-06-29 18:33:18 +0300

    Add Maven 4.0.0-alpha-7

--- src/main/scala/io/sdkman/changelogs/MavenMigration.scala
+++ src/main/scala/io/sdkman/changelogs/MavenMigration.scala
@@ -6,16 +6,15 @@ import com.mongodb.client.MongoDatabase
 @ChangeLog(order = "013")
 class MavenMigration {
   @ChangeSet(
-    order = "031",
-    id = "031-maven-3.9.3",
+    order = "032",
+    id = "032-maven-4.0.0-alpha-7",
     author = "aalmiray"
   )
-  def migration031(implicit db: MongoDatabase): Unit =
+  def migration032(implicit db: MongoDatabase): Unit =
     Version(
       "maven",
-      "3.9.3",
-      "https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.9.3/apache-maven-3.9.3-bin.zip"
+      "4.0.0-alpha-7",
+      "https://dlcdn.apache.org/maven/maven-4/4.0.0-alpha-7/binaries/apache-maven-4.0.0-alpha-7-bin.zip"
     ).validate()
       .insert()
-      .asCandidateDefault()
 }
