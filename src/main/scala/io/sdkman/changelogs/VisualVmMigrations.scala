0d6d7262da1b7df1973d2695931e3034979cca46
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2023-09-19 13:59:33 -0600

    Add VisualVM 2.1.7 (#662)

--- src/main/scala/io/sdkman/changelogs/VisualVmMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/VisualVmMigrations.scala
@@ -6,15 +6,15 @@ import com.mongodb.client.MongoDatabase
 @ChangeLog(order = "009")
 class VisualVmMigrations {
   @ChangeSet(
-    order = "017",
-    id = "017-add_visualvm_2_1_6",
+    order = "018",
+    id = "018-add_visualvm_2_1_7",
     author = "eddumelendez"
   )
   def migration(implicit db: MongoDatabase): Unit = {
     Version(
       "visualvm",
-      "2.1.6",
-      "https://github.com/oracle/visualvm/releases/download/2.1.6/visualvm_216.zip"
+      "2.1.7",
+      "https://github.com/oracle/visualvm/releases/download/2.1.7/visualvm_217.zip"
     ).validate()
       .insert()
       .asCandidateDefault()
