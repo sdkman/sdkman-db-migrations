073e1606308711775f1c96625a8ad9b8d4f631ad
Andres Almiray <aalmiray@gmail.com>
2023-10-05 10:45:43 -0600

    Add Maven 3.9.5 (#665)

--- src/main/scala/io/sdkman/changelogs/MavenMigration.scala
+++ src/main/scala/io/sdkman/changelogs/MavenMigration.scala
@@ -6,15 +6,15 @@ import com.mongodb.client.MongoDatabase
 @ChangeLog(order = "013")
 class MavenMigration {
   @ChangeSet(
-    order = "033",
-    id = "033-maven-3.9.4",
+    order = "034",
+    id = "034-maven-3.9.5",
     author = "aalmiray"
   )
-  def migration033(implicit db: MongoDatabase): Unit =
+  def migration034(implicit db: MongoDatabase): Unit =
     Version(
       "maven",
-      "3.9.4",
-      "https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.9.4/apache-maven-3.9.4-bin.zip"
+      "3.9.5",
+      "https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.9.5/apache-maven-3.9.5-bin.zip"
     ).validate()
       .insert()
       .asCandidateDefault()
