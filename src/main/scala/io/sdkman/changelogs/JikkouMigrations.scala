d2733af75991c34e9f391ee86554cc854a4fbd3e
Oliver Weiler <oliver@sdkman.io>
2023-10-09 15:42:50 +0200

    chore: Replace scalafmt with checkScalafmt (#668)

--- src/main/scala/io/sdkman/changelogs/JikkouMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/JikkouMigrations.scala
@@ -17,7 +17,7 @@ class JikkouMigrations {
       name = "Jikkou",
       description =
         "Jikkou is a command-line client for efficient management, automation and provisioning of resources for Kafka, Schema Registry, etc. " +
-        "Inspired by kubectl and the Kubernetes resource definition files, it offers an intuitive, user-friendly approach to configuration management.",
+          "Inspired by kubectl and the Kubernetes resource definition files, it offers an intuitive, user-friendly approach to configuration management.",
       websiteUrl = "https://streamthoughts.github.io/jikkou/"
     ).insert()
 }
