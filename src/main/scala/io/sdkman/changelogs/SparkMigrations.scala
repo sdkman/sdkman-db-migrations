2397cb6556721a54f016e55c6d178615a263f51e
Chris Hebert <8251796+cphbrt@users.noreply.github.com>
2023-11-03 14:25:05 -0500

    Add Spark 3.4.1 and 3.5.0

--- src/main/scala/io/sdkman/changelogs/SparkMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/SparkMigrations.scala
@@ -48,4 +48,33 @@ class SparkMigrations {
       .insert()
       .asCandidateDefault()
   }
+
+  @ChangeSet(
+    order = "026",
+    id = "026-add_spark_3.4.1",
+    author = "cphbrt"
+  )
+  def migration026(implicit db: MongoDatabase) = {
+    Version(
+      "spark",
+      "3.4.1",
+      "https://archive.apache.org/dist/spark/spark-3.4.1/spark-3.4.1-bin-hadoop3.tgz"
+    ).validate()
+      .insert()
+  }
+
+  @ChangeSet(
+    order = "027",
+    id = "027-add_spark_3.5.0",
+    author = "cphbrt"
+  )
+  def migration027(implicit db: MongoDatabase) = {
+    Version(
+      "spark",
+      "3.5.0",
+      "https://archive.apache.org/dist/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz"
+    ).validate()
+      .insert()
+      .asCandidateDefault()
+  }
 }
