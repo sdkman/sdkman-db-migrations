f5ce07ed4fc691484c36d6a351a129f32e561fdb
Andres Almiray <aalmiray@gmail.com>
2024-01-18 05:56:33 -0600

    Add Maven 4.0.0-alpha-12 (#681)

--- src/main/scala/io/sdkman/changelogs/MavenMigration.scala
+++ src/main/scala/io/sdkman/changelogs/MavenMigration.scala
@@ -6,15 +6,15 @@ import com.mongodb.client.MongoDatabase
 @ChangeLog(order = "013")
 class MavenMigration {
   @ChangeSet(
-    order = "038",
-    id = "038-maven-4.0.0-alpha-10",
+    order = "039",
+    id = "039-maven-4.0.0-alpha-12",
     author = "aalmiray"
   )
-  def migration038(implicit db: MongoDatabase): Unit =
+  def migration039(implicit db: MongoDatabase): Unit =
     Version(
       "maven",
-      "4.0.0-alpha-10",
-      "https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/4.0.0-alpha-10/apache-maven-4.0.0-alpha-10-bin.zip"
+      "4.0.0-alpha-12",
+      "https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/4.0.0-alpha-12/apache-maven-4.0.0-alpha-12-bin.zip"
     ).validate()
       .insert()
 }
