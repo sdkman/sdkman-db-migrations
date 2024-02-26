428eb14b95618e82f46d627700e34846fb849e44
jandroav <jandroav@icloud.com>
2024-02-26 13:08:11 +0100

    chore(LiquibaseMigrations.scala): update @ChangeLog order to "085" for consistency refactor(LiquibaseMigrations.scala): update Liquibase description for clarity and conciseness

--- src/main/scala/io/sdkman/changelogs/LiquibaseMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/LiquibaseMigrations.scala
@@ -1,11 +1,11 @@
-@ChangeLog(order = "036")
+@ChangeLog(order = "085")
 class LiquibaseMigrations {
     @ChangeSet(order = "001", id = "001_add_liquibase_1_2_3", author = "jandroav")
     def migration001(implicit db: MongoDatabase) = {
         Candidate(
           candidate = "liquibase",
           name = "Liquibase",
-          description = "Innovate faster with database DevOps. Automated, secure, and compliant database change management pipelines that accelerate delivery and reduce toil",
+          description = "Liquibase is an open-source database-independent library for tracking, managing and applying database schema changes.",
           websiteUrl = "https://liquibase.com/",
           distribution = "UNIVERSAL"
         ).insert()
