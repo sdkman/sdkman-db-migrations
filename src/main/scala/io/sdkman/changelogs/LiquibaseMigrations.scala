0cf6d890e1d14e86c8e51213af3e4ec3c102ca28
jandroav <jandroav@icloud.com>
2024-02-26 13:20:45 +0100

    refactor(LiquibaseMigrations.scala): reformat code for better readability and consistency in indentation and spacing

--- src/main/scala/io/sdkman/changelogs/LiquibaseMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/LiquibaseMigrations.scala
@@ -1,13 +1,14 @@
 @ChangeLog(order = "085")
 class LiquibaseMigrations {
-    @ChangeSet(order = "001", id = "001_add_liquibase_1_2_3", author = "jandroav")
-    def migration001(implicit db: MongoDatabase) = {
-        Candidate(
-          candidate = "liquibase",
-          name = "Liquibase",
-          description = "Liquibase is an open-source database-independent library for tracking, managing and applying database schema changes.",
-          websiteUrl = "https://liquibase.com/",
-          distribution = "UNIVERSAL"
-        ).insert()
-    }
+  @ChangeSet(order = "001", id = "001_add_liquibase_1_2_3", author = "jandroav")
+  def migration001(implicit db: MongoDatabase) = {
+    Candidate(
+      candidate = "liquibase",
+      name = "Liquibase",
+      description =
+        "Liquibase is an open-source database-independent library for tracking, managing and applying database schema changes.",
+      websiteUrl = "https://liquibase.com/",
+      distribution = "UNIVERSAL"
+    ).insert()
+  }
 }
