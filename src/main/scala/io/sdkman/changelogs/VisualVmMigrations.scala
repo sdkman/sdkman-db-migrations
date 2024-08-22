19f406c797491cc4ca7c44d09b097b6c1ac226c4
Oliver Weiler <oliver@sdkman.io>
2024-08-22 14:44:16 +0200

    chore: Add VisualVM 2.1.9 (#705)

--- src/main/scala/io/sdkman/changelogs/VisualVmMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/VisualVmMigrations.scala
@@ -6,15 +6,15 @@ import com.mongodb.client.MongoDatabase
 @ChangeLog(order = "009")
 class VisualVmMigrations {
   @ChangeSet(
-    order = "019",
-    id = "019-add_visualvm_2_1_8",
-    author = "eddumelendez"
+    order = "020",
+    id = "020-add_visualvm_2_1_9",
+    author = "helpermethod"
   )
-  def migration(implicit db: MongoDatabase): Unit = {
+  def migration020(implicit db: MongoDatabase): Unit = {
     Version(
       "visualvm",
-      "2.1.8",
-      "https://github.com/oracle/visualvm/releases/download/2.1.8/visualvm_218.zip"
+      "2.1.9",
+      "https://github.com/oracle/visualvm/releases/download/2.1.9/visualvm_219.zip"
     ).validate()
       .insert()
       .asCandidateDefault()
