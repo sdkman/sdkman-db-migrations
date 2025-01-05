b85dfac85cf888d641b8acb8aed8af1cb235acf9
Jerome Angibaud <jerome_angibaud@yahoo.fr>
2025-01-05 13:15:48 +0100

    Update src/main/scala/io/sdkman/changelogs/JekaMigrations.scala
    Co-authored-by: Marco Vermeulen <marco.vermeulen@protonmail.com>

--- src/main/scala/io/sdkman/changelogs/JekaMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/JekaMigrations.scala
@@ -14,7 +14,7 @@ class MicronautMigrations {
   def migration001(implicit db: MongoDatabase): Candidate = {
     Candidate(
       candidate = "jeka",
-      name = "Jeka",
+      name = "JeKa",
       description =
         "JeKa is a modern build tool for Java, designed to simplify and streamline your development experience. It reshapes Java application building for a generation of developers seeking lightweight, accessible, yet powerful solutions."
       websiteUrl = "http://jeka.dev"
