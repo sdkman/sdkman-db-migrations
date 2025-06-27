00285c6326ea2d4b58299162f867c76d887ee587
Henning Hoefer <hoeferh@gmail.com>
2025-06-27 10:29:34 +0200

    Add Maven 4.0.0-rc4

--- src/main/scala/io/sdkman/changelogs/MavenMigration.scala
+++ src/main/scala/io/sdkman/changelogs/MavenMigration.scala
@@ -6,15 +6,15 @@ import com.mongodb.client.MongoDatabase
 @ChangeLog(order = "013")
 class MavenMigration {
   @ChangeSet(
-    order = "045",
-    id = "045-maven-4.0.0-rc-3",
-    author = "ascheman"
+    order = "046",
+    id = "046-maven-4.0.0-rc-4",
+    author = "hho"
   )
-  def migration045(implicit db: MongoDatabase): Unit =
+  def migration046(implicit db: MongoDatabase): Unit =
     Version(
       "maven",
-      "4.0.0-rc-3",
-      "https://dlcdn.apache.org/maven/maven-4/4.0.0-rc-3/binaries/apache-maven-4.0.0-rc-3-bin.zip"
+      "4.0.0-rc-4",
+      "https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/4.0.0-rc-4/apache-maven-4.0.0-rc-4-bin.zip"
     ).validate()
       .insert()
 }
