84de104d54eafec2bca7b12da8514b2238c7bfc7
Thanos Stratikopoulos <tstratiko@gmail.com>
2025-11-07 18:01:30 +0200

    Add TornadoVM candidate

---
+++ src/main/scala/io/sdkman/changelogs/TornadoVMCandidateMigration.scala
@@ -0,0 +1,20 @@
+package io.sdkman.changelogs.java
+
+import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
+import com.mongodb.client.MongoDatabase
+import io.sdkman.db.MongoConnector
+
+@ChangeLog(order = "050")
+class TornadoVMMigrations {
+
+    @ChangeSet(order = "001", id = "001_add_tornadovm_candidate", author = "stratika")
+    def migration001(implicit db: MongoDatabase) = {
+        Candidate(
+            candidate = "tornadovm",
+            name = "TornadoVM",
+            description = "TornadoVM is a plug-in to OpenJDK and GraalVM that allows Java programmers to automatically run on heterogeneous hardware (GPUs, FPGAs, multi-core CPUs).",
+            websiteUrl = "https://www.tornadovm.org/"
+            distribution = "PLATFORM_SPECIFIC"
+        ).insert()
+    }
+}
