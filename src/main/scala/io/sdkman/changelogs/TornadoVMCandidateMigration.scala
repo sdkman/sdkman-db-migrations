c1fcdb7ddd9c84266970086ed5750d47c6a4b516
Thanos Stratikopoulos <tstratiko@gmail.com>
2025-12-02 20:43:14 +0200

    fix: scalafmt

--- src/main/scala/io/sdkman/changelogs/TornadoVMCandidateMigration.scala
+++ src/main/scala/io/sdkman/changelogs/TornadoVMCandidateMigration.scala
@@ -7,14 +7,19 @@ import io.sdkman.db.MongoConnector
 @ChangeLog(order = "050")
 class TornadoVMMigrations {
 
-    @ChangeSet(order = "001", id = "001_add_tornadovm_candidate", author = "stratika")
-    def migration001(implicit db: MongoDatabase) = {
-        Candidate(
-            candidate = "tornadovm",
-            name = "TornadoVM",
-            description = "TornadoVM is a plug-in to OpenJDK and GraalVM that allows Java programmers to automatically run on heterogeneous hardware (GPUs, FPGAs, multi-core CPUs).",
-            websiteUrl = "https://www.tornadovm.org/"
-            distribution = "PLATFORM_SPECIFIC"
-        ).insert()
-    }
+  @ChangeSet(
+    order = "001",
+    id = "001_add_tornadovm_candidate",
+    author = "stratika"
+  )
+  def migration001(implicit db: MongoDatabase) = {
+    Candidate(
+      candidate = "tornadovm",
+      name = "TornadoVM",
+      description =
+        "TornadoVM is a plug-in to OpenJDK and GraalVM that allows Java programmers to automatically run on heterogeneous hardware (GPUs, FPGAs, multi-core CPUs).",
+      websiteUrl = "https://www.tornadovm.org/" distribution =
+        "PLATFORM_SPECIFIC"
+    ).insert()
+  }
 }
