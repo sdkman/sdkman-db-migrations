3e3320985080cf8b87fc19cd260b9d558007e07a
Nek-12 <vaizin.nikita@gmail.com>
2026-01-20 21:49:34 +0100

    Add ksrc candidate

---
+++ src/main/scala/io/sdkman/changelogs/KsrcMigrations.scala
@@ -0,0 +1,21 @@
+package io.sdkman.changelogs
+
+import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
+import com.mongodb.client.MongoDatabase
+
+@ChangeLog(order = "090")
+class KsrcMigrations {
+  @ChangeSet(
+    order = "001",
+    id = "001_add_ksrc_candidate",
+    author = "respawn-app"
+  )
+  def migration001(implicit db: MongoDatabase) =
+    Candidate(
+      candidate = "ksrc",
+      name = "ksrc",
+      description = "Let your AI agents search and read 3rd-party Kotlin dependency sources",
+      websiteUrl = "https://github.com/respawn-app/ksrc",
+      distribution = "PLATFORM_SPECIFIC"
+    ).insert()
+}
