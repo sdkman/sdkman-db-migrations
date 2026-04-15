14e9fa793ae3e5e39650b42c85e9b9c337649a22
jfarcand <jfarcand@apache.org>
2026-04-15 16:01:28 -0400

    fix: bump @ChangeLog order to 090 (highest existing was 089)

--- src/main/scala/io/sdkman/changelogs/AtmosphereMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/AtmosphereMigrations.scala
@@ -3,7 +3,7 @@ package io.sdkman.changelogs
 import com.github.mongobee.changeset.{ChangeLog, ChangeSet}
 import com.mongodb.client.MongoDatabase
 
-@ChangeLog(order = "075")
+@ChangeLog(order = "090")
 class AtmosphereMigrations {
 
   @ChangeSet(
