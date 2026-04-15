3cb88191db04603ba2b3f7cbd66ccee156bfa457
aganisatria <agani.satria@traveloka.com>
2026-04-15 10:34:35 +0700

    fix review

--- src/main/scala/io/sdkman/changelogs/SparkMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/SparkMigrations.scala
@@ -8,11 +8,11 @@ import org.bson.Document
 class SparkMigrations {
 
   @ChangeSet(
-    order = "031",
-    id = "031-latest-spark-versions",
-    author = "aganisatria"
+    order = "030",
+    id = "030-latest-spark-versions",
+    author = "noobzik"
   )
-  def migration031(implicit db: MongoDatabase): Document = {
+  def migration030(implicit db: MongoDatabase): Document = {
     Map(
       "2.2.2"          -> "hadoop2.7",
       "2.2.3"          -> "hadoop2.7",
@@ -28,13 +28,32 @@ class SparkMigrations {
       "3.5.1"          -> "hadoop3",
       "3.5.2"          -> "hadoop3",
       "3.5.3"          -> "hadoop3",
+      "4.0.0-preview1" -> "hadoop3",
+      "4.0.0-preview2" -> "hadoop3"
+    ).foreach {
+      case (version, distribution) =>
+        Version(
+          candidate = "spark",
+          version = version,
+          url =
+            s"https://archive.apache.org/dist/spark/spark-$version/spark-$version-bin-$distribution.tgz"
+        ).validate().insert()
+    }
+    setCandidateDefault("spark", "3.5.3")
+  }
+
+  @ChangeSet(
+    order = "031",
+    id = "031-latest-spark-versions",
+    author = "aganisatria"
+  )
+  def migration031(implicit db: MongoDatabase): Document = {
+    Map(
       "3.5.4"          -> "hadoop3",
       "3.5.5"          -> "hadoop3",
       "3.5.6"          -> "hadoop3",
       "3.5.7"          -> "hadoop3",
       "3.5.8"          -> "hadoop3",
-      "4.0.0-preview1" -> "hadoop3",
-      "4.0.0-preview2" -> "hadoop3",
       "4.0.0"          -> "hadoop3",
       "4.0.1"          -> "hadoop3",
       "4.0.2"          -> "hadoop3",
