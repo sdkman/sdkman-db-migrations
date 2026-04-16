2e5275e09eb4d3b1b59e30d0be05b02bc2dd4597
Thorsten Heit <theit@gmx.de>
2026-04-16 15:09:09 +0200

    Add Ant 1.10.16 and 1.10.17

--- src/main/scala/io/sdkman/changelogs/AntMigration.scala
+++ src/main/scala/io/sdkman/changelogs/AntMigration.scala
@@ -6,15 +6,30 @@ import com.mongodb.client.MongoDatabase
 @ChangeLog(order = "032")
 class AntMigration {
   @ChangeSet(
-    order = "019",
-    id = "019-add_ant_1.10.15",
-    author = "helpermethod"
+    order = "020",
+    id = "020-add_ant_1.10.16",
+    author = "theit"
   )
-  def migration019(implicit db: MongoDatabase): Unit = {
+  def migration020(implicit db: MongoDatabase): Unit = {
     Version(
       "ant",
-      "1.10.15",
-      "https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.15-bin.zip"
+      "1.10.16",
+      "https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.16-bin.zip"
+    ).validate()
+      .insert()
+      .asCandidateDefault()
+  }
+
+  @ChangeSet(
+    order = "021",
+    id = "021-add_ant_1.10.17",
+    author = "theit"
+  )
+  def migration021(implicit db: MongoDatabase): Unit = {
+    Version(
+      "ant",
+      "1.10.17",
+      "https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.17-bin.zip"
     ).validate()
       .insert()
       .asCandidateDefault()
