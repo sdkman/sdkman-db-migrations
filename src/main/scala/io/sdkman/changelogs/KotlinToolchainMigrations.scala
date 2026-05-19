96c62616403b2c6e4658de6fb9771dc4deeca408
Anton Prokhorov <anton.prokhorov@jetbrains.com>
2026-05-19 15:19:20 +0200

    Add first `kotlintoolchain` version

--- src/main/scala/io/sdkman/changelogs/KotlinToolchainMigrations.scala
+++ src/main/scala/io/sdkman/changelogs/KotlinToolchainMigrations.scala
@@ -22,4 +22,19 @@ class KotlinToolchainMigrations {
       websiteUrl = "https://kotlin-toolchain.org",
       distribution = "UNIVERSAL"
     ).insert()
+
+  @ChangeSet(
+    order = "002",
+    id = "002_add_kotlintoolchain_0_11_0",
+    author = "singleton11"
+  )
+  def migration002(implicit db: MongoDatabase): Unit =
+    Version(
+      candidate = "kotlintoolchain",
+      version = "0.11.0",
+      url =
+        "https://packages.jetbrains.team/maven/p/amper/amper/org/jetbrains/kotlin/kotlin-cli-sdkman/0.11.0/kotlin-cli-sdkman-0.11.0.zip"
+    ).validate()
+      .insert()
+      .asCandidateDefault()
 }
